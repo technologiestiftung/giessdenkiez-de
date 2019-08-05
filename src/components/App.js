b342344223eed751205a03595a400ac020bc9f56
fdnklg <fabian.dinklage@me.com>
2019-08-05 21:46:41 +0200

    basic boilerplate setup

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class AppContainer extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+        }
+    }  
+
+    render() {
+        return (
+            <div>
+                I'm working!!!!
+            </div>
+        ) 
+    }
+}
+
+export default AppContainer;
+// export default connect(mapStateToProps)(AppContainer);
---
+++ src/index.js
@@ -0,0 +1,32 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './Root';
+
+// import { Provider } from "react-redux";
+// import store from './store/index.js';
+
+// import FormContainer from "./js/components/container/FormContainer.jsx";
+
+// import { setTime } from './store/actions/index.js';
+
+// window.store = store;
+// window.setTime = setTime;
+
+function startApp() {
+    const rootElement = document.getElementById('app');
+
+    function renderApp(RootComponent) {
+        ReactDOM.render(
+            // <Provider store={store}>
+                <RootComponent/>    
+            // </Provider>
+            ,rootElement);
+    }
+
+    // Mount the react-app
+    renderApp(Root);
+}
+
+startApp();
