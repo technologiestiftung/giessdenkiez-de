241e2870e640cdc7b0c5592a157e82463b9b44c0
fdnklg <fabian.dinklage@me.com>
2019-09-03 13:35:31 +0200

    loading state selected tree

--- src/components/SelectedTree/index.js
+++ src/components/SelectedTree/index.js
@@ -8,6 +8,64 @@ import ButtonWater from '../ButtonWater/index';
 
 import content from '../../assets/content';
 
+const FilterLoadingDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    z-index: 3;
+    height: auto;
+    margin-top: 10px;
+    padding: ${props => props.theme.spacingM};
+    width: ${props => props.theme.sidebarTileWidth};
+    border-radius: ${props => props.theme.borderRadiusS};
+    margin-bottom: 10px;
+    background: white;
+    box-shadow: 0px 2px 3px 0px rgba(44,48,59,0.1);
+`
+
+const Spinner = styled.div`
+    width: 15px;
+    height: 15px;
+    transform: translateY(3px);
+    margin-right: 10px;
+
+    position: relative;
+    float: left;
+
+    .double-bounce1, .double-bounce2 {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #37DE8A;
+        opacity: 0.6;
+        position: absolute;
+        top: 0;
+        left: 0;
+        
+        -webkit-animation: sk-bounce 2.0s infinite ease-in-out;
+        animation: sk-bounce 2.0s infinite ease-in-out;
+    }
+
+    .double-bounce2 {
+        -webkit-animation-delay: -1.0s;
+        animation-delay: -1.0s;
+    }
+
+    @-webkit-keyframes sk-bounce {
+        0%, 100% { -webkit-transform: scale(0.0) }
+        50% { -webkit-transform: scale(1.0) }
+    }
+
+    @keyframes sk-bounce {
+        0%, 100% { 
+            transform: scale(0.0);
+            -webkit-transform: scale(0.0);
+        } 50% { 
+            transform: scale(1.0);
+            -webkit-transform: scale(1.0);
+        }
+    }
+`
+
 const SelectedTreeDiv = styled.div`
     display: flex;
     flex-direction: column;
@@ -121,9 +179,13 @@ class SelectedTree extends React.Component {
     render() {
         if (this.props.selectedTreeDataLoading) {
             return (
-                <SelectedTreeDiv>
-                    <span className="pulsing">Lade Informationen zum Baum.</span>
-                </SelectedTreeDiv>
+                <FilterLoadingDiv>
+                    <Spinner>
+                        <div className="double-bounce1"></div>
+                        <div className="double-bounce2"></div>
+                    </Spinner>
+                    <span>Lade Informationen zum Baum.</span>
+                </FilterLoadingDiv>
             )
         } else if (!this.props.selectedTreeData) {
                 return (
