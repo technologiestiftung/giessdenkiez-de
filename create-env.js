a0e1fb29bcda259b74cce2f78ef4971cae9fa9d5
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-05-05 17:48:59 +0200

    setup(auth0): Adds a new tenant for auth0 creates env vars file on netlify removes auth0_config.json (still needs change of tenant in api)

--- create-env.js
+++ create-env.js
@@ -1,2 +1,9 @@
-const fs = require('fs')
-fs.writeFileSync('./.env', `API_KEY=${process.env.API_KEY}\n`)
+const fs = require('fs');
+const envVars = `
+# these are only generated on netlify deploy
+API_KEY=${process.env.API_KEY}\n
+AUTH0_DOMAIN=${AUTH0_DOMAIN}\n
+AUTH0_CLIENT_ID=${AUTH0_CLIENT_ID}\n
+AUTH0_AUDIENCE=${AUTH0_AUDIENCE}\n
+`;
+fs.writeFileSync('./.env', envVars);
