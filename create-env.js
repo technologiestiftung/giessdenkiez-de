ebb3445cb923174496bcc62ae26252cf63503732
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-05-05 18:16:02 +0200

    fix(netlify build): Adds missing process.env (facepalm)

--- create-env.js
+++ create-env.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const envVars = `
 # these are only generated on netlify deploy
 API_KEY=${process.env.API_KEY}\n
-AUTH0_DOMAIN=${AUTH0_DOMAIN}\n
-AUTH0_CLIENT_ID=${AUTH0_CLIENT_ID}\n
-AUTH0_AUDIENCE=${AUTH0_AUDIENCE}\n
+AUTH0_DOMAIN=${process.env.AUTH0_DOMAIN}\n
+AUTH0_CLIENT_ID=${process.env.AUTH0_CLIENT_ID}\n
+AUTH0_AUDIENCE=${process.env.AUTH0_AUDIENCE}\n
 `;
 fs.writeFileSync('./.env', envVars);
