197029de3896dbb5443bd4f28d7847b03054c515
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-05-06 09:41:08 +0200

    fix: remove React Helmet content

--- src/Root.js
+++ src/Root.js
@@ -1,14 +1,6 @@
 import React from 'react';
-import MetaTags from './components/MetaTags';
 import App from './components/App';
 
-const Root = () => {
-  return(
-    <>
-      <MetaTags />
-      <App />
-    </>
-  )
-};
+const Root = () => <App />;
 
 export default Root;
