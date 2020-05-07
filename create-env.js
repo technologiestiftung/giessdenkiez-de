ab46ea958bc0feae9aad80fb59f9fc6eedd6005c
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-05-07 19:52:21 +0200

    Add env for user data api

--- create-env.js
+++ create-env.js
@@ -5,5 +5,6 @@ API_KEY=${process.env.API_KEY}\n
 AUTH0_DOMAIN=${process.env.AUTH0_DOMAIN}\n
 AUTH0_CLIENT_ID=${process.env.AUTH0_CLIENT_ID}\n
 AUTH0_AUDIENCE=${process.env.AUTH0_AUDIENCE}\n
+USER_DATA_API_URL=${process.env.USER_DATA_API_URL}\n
 `;
 fs.writeFileSync('./.env', envVars);
