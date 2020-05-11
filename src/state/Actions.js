8a22784ea54437cd6c89c12493aa69171c0b2b5c
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-05-11 16:36:29 +0200

    fix import of history

--- src/state/Actions.js
+++ src/state/Actions.js
@@ -1,27 +1,36 @@
-import { dsv as d3Dsv, easeCubic as d3EaseCubic, json as D3Json } from "d3";
-import history from "../../history";
-import { createAPIUrl, fetchAPI, flatten, createGeojson, createCSVJson } from "../utils";
-import { FlyToInterpolator } from "react-map-gl";
+import { dsv as d3Dsv, easeCubic as d3EaseCubic, json as D3Json } from 'd3';
+import history from '../history';
+import {
+  createAPIUrl,
+  fetchAPI,
+  flatten,
+  createGeojson,
+  createCSVJson,
+} from '../utils';
+import { FlyToInterpolator } from 'react-map-gl';
 
-export const loadTrees = (Store) => async () => {
-  const dataUrlLocal = "../../public/data/trees.csv.gz"
-  const dataUrl = "https://tsb-trees.s3.eu-central-1.amazonaws.com/trees.csv.gz"
+export const loadTrees = Store => async () => {
+  const dataUrlLocal = '../../public/data/trees.csv.gz';
+  const dataUrl =
+    'https://tsb-trees.s3.eu-central-1.amazonaws.com/trees.csv.gz';
 
-  d3Dsv(",", dataUrl)
-    .then((data) => {
-      const geojson = createGeojson(data);
-      Store.setState({ data: geojson, isLoading: false, });
-    });
-}
+  d3Dsv(',', dataUrl).then(data => {
+    const geojson = createGeojson(data);
+    Store.setState({ data: geojson, isLoading: false });
+  });
+};
 
 export const setAgeRange = (state, payload) => {
   return {
-    ageRange: payload
-  }
-}
+    ageRange: payload,
+  };
+};
 
-export const loadCommunityData = (Store) => async () => {
-  const fetchCommunityDataUrl = createAPIUrl(Store.getState(), `/get-watered-and-adopted`);
+export const loadCommunityData = Store => async () => {
+  const fetchCommunityDataUrl = createAPIUrl(
+    Store.getState(),
+    `/get-watered-and-adopted`
+  );
   const communityData = await fetchAPI(fetchCommunityDataUrl);
 
   let obj = {};
@@ -31,27 +40,27 @@ export const loadCommunityData = (Store) => async () => {
     obj[item[0]] = {
       adopted: item[1] == 1 ? true : false,
       watered: item[2] == 1 ? true : false,
-    }
-  })
+    };
+  });
 
-  Store.setState({ communityData: Object.assign({}, obj)});
-}
+  Store.setState({ communityData: Object.assign({}, obj) });
+};
 
-export const loadData = (Store) => async () => {
+export const loadData = Store => async () => {
   Store.setState({ isLoading: true });
   let geojson = [];
 
   const dataUrl =
-    "https://tsb-trees.s3.eu-central-1.amazonaws.com/weather_light.geojson.gz";
+    'https://tsb-trees.s3.eu-central-1.amazonaws.com/weather_light.geojson.gz';
 
   fetch(dataUrl)
-    .then((res) => res.json())
-    .then((r) => Store.setState({ rainGeojson: r }));
+    .then(res => res.json())
+    .then(r => Store.setState({ rainGeojson: r }));
 
-  const pumps_data = require("../../data/pumps.geojson");
-  const pumps = fetch("../../data/pumps.geojson")
+  const pumps_data = require('../../data/pumps.geojson');
+  const pumps = fetch('../../data/pumps.geojson')
     .then(r => r.json())
-    .then(r => Store.setState({ pumps: r }))
+    .then(r => Store.setState({ pumps: r }));
 };
 
 export const setAppState = (state, payload) => {
@@ -110,9 +119,9 @@ function checkStatus(response) {
   }
 }
 
-export const getWateredTrees = (Store) => async () => {
+export const getWateredTrees = Store => async () => {
   Store.setState({ isLoading: true });
-  const url = createAPIUrl(Store.getState(), "/get-watered-trees");
+  const url = createAPIUrl(Store.getState(), '/get-watered-trees');
   const res = await fetchAPI(url);
 
   return {
@@ -120,7 +129,7 @@ export const getWateredTrees = (Store) => async () => {
   };
 };
 
-export const getTree = (Store) => async (id) => {
+export const getTree = Store => async id => {
   const url = createAPIUrl(Store.getState(), `/get-tree?id=${id}`);
   const urlWatered = createAPIUrl(
     Store.getState(),
@@ -143,13 +152,13 @@ export const removeSelectedTree = (state, payload) => {
   };
 };
 
-export const getTreeByAge = (Store) => async (state, start, end) => {
-  Store.setState({ selectedTreeState: "LOADING" });
+export const getTreeByAge = Store => async (state, start, end) => {
+  Store.setState({ selectedTreeState: 'LOADING' });
   const url = createAPIUrl(state, `/get-tree-by-age?start=${start}&end=${end}`);
 
-  const res = await fetchAPI(url).then((r) => {
+  const res = await fetchAPI(url).then(r => {
     Store.setState({
-      selectedTreeState: "LOADED",
+      selectedTreeState: 'LOADED',
       selectedTrees: r.data,
     });
   });
@@ -164,7 +173,7 @@ const setDetailRouteWithListPath = (state, treeId) => {
   history.push(nextLocation);
 };
 
-export default (Store) => ({
+export default Store => ({
   loadData: loadData(Store),
   setDataView,
   getWateredTrees: getWateredTrees(Store),
