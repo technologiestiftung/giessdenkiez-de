d31b01d4c425a290bf48b8fd14626461edb13ed8
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-05-12 13:54:21 +0200

    Add error overlay to dev server

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,8 +1,18 @@
 const merge = require('webpack-merge');
 const common = require('./common.config');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
 module.exports = merge(common, {
   devtool: 'inline-source-map',
   mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+    compress: true,
+    overlay: {
+      errors: true,
+      warnings: false,
+    },
+  },
+
   optimization: {
     splitChunks: {
       // include all types of chunks
@@ -17,4 +27,5 @@ module.exports = merge(common, {
       },
     },
   },
+  plugins: [new ErrorOverlayPlugin()],
 });
