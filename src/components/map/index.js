b6c5a50adb52187fd9b3ecb3b2778c228156469a
sebastian-meier <contact@sebastianmeier.eu>
2020-05-19 14:41:52 +0200

    slow vector tiles

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -7,6 +7,8 @@ import {
   /*GeolocateControl,*/ NavigationControl,
 } from 'react-map-gl';
 import DeckGL, { GeoJsonLayer, TileLayer } from 'deck.gl';
+import {VectorTile} from '@mapbox/vector-tile';
+import Protobuf from 'pbf';
 import Store from '../../state/Store';
 import { wateredTreesSelector } from '../../state/Selectors';
 import {
@@ -79,80 +81,100 @@ class DeckGLMap extends React.Component {
       const layers = [
         new TileLayer({
           id: 'geojson',
-          data: 'https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidGVjaG5vbG9naWVzdGlmdHVuZyIsImEiOiJjanZubXFzc3YxOTk3NGFxanNxMHdkc3Z0In0.cvnIEVF97kQljPfbB8nUZg',
-          opacity: 1,
-          getLineWidth: info => {
-            const { selectedTree } = this.props;
-            const id = info.properties['id'];
-
-            if (selectedTree) {
-              if (id === selectedTree.id) {
-                return 2;
-              } else {
-                return 0;
-              }
-            } else {
-              return 0;
-            }
+          minZoom: 11,
+          maxZoom: 17,
+          getTileData: ({x, y, z}) => {
+            const mapSource = `https://a.tiles.mapbox.com/v4/technologiestiftung.trees_v2/${z}/${x}/${y}.vector.pbf?access_token=pk.eyJ1IjoidGVjaG5vbG9naWVzdGlmdHVuZyIsImEiOiJjanZubXFzc3YxOTk3NGFxanNxMHdkc3Z0In0.cvnIEVF97kQljPfbB8nUZg`;
+            return fetch(mapSource)
+              .then(response => response.arrayBuffer())
+              .then(buffer => {
+                const tile = new VectorTile(new Protobuf(buffer));
+                const features = [];
+                for (const layerName in tile.layers) {
+                  const vectorTileLayer = tile.layers[layerName];
+                  for (let i = 0; i < vectorTileLayer.length; i++) {
+                    const vectorTileFeature = vectorTileLayer.feature(i);
+                    const feature = vectorTileFeature.toGeoJSON(x, y, z);
+                    features.push(feature);
+                  }
+                }
+                return features;
+              });
           },
-          getLineColor: [247, 105, 6, 255],
-          visible: treesVisible,
-          filled: true,
-          parameters: {
-            depthTest: false,
-          },
-          pickable: true,
-          getRadius: 3,
-          type: 'circle',
-          pointRadiusMinPixels: 0.5,
-          autoHighlight: true,
-          highlightColor: [200, 200, 200, 255],
-          transitions: {
-            getFillColor: 500,
-          },
-          getFillColor: (info, i) => {
-            const {
-              // wateredTrees,
-              // AppState,
-              ageRange,
-              dataView,
-              communityData,
-            } = this.props;
-            const { properties } = info;
-            const { id, radolan_sum, age } = properties;
+          // data: 'https://api.mapbox.com/v4/technologiestiftung.trees_v2/{z}/{x}/{y}.pbf?access_token=pk.eyJ1IjoidGVjaG5vbG9naWVzdGlmdHVuZyIsImEiOiJjanZubXFzc3YxOTk3NGFxanNxMHdkc3Z0In0.cvnIEVF97kQljPfbB8nUZg',
+          // opacity: 1,
+          // getLineWidth: info => {
+          //   const { selectedTree } = this.props;
+          //   const id = info.properties['id'];
 
-            if (dataView === 'watered' && communityData[id]) {
-              return communityData[id].watered
-                ? [0, 0, 255, 200]
-                : [0, 0, 0, 0];
-            }
+          //   if (selectedTree) {
+          //     if (id === selectedTree.id) {
+          //       return 2;
+          //     } else {
+          //       return 0;
+          //     }
+          //   } else {
+          //     return 0;
+          //   }
+          // },
+          // getLineColor: [247, 105, 6, 255],
+          // visible: treesVisible,
+          // filled: true,
+          // parameters: {
+          //   depthTest: false,
+          // },
+          // pickable: true,
+          // getRadius: 3,
+          // type: 'circle',
+          // pointRadiusMinPixels: 0.5,
+          // autoHighlight: true,
+          // highlightColor: [200, 200, 200, 255],
+          // transitions: {
+          //   getFillColor: 500,
+          // },
+          // getFillColor: (info, i) => {
+          //   const {
+          //     // wateredTrees,
+          //     // AppState,
+          //     ageRange,
+          //     dataView,
+          //     communityData,
+          //   } = this.props;
+          //   const { properties } = info;
+          //   const { id, radolan_sum, age } = properties;
 
-            if (dataView === 'adopted' && communityData[id]) {
-              return communityData[id].adopted
-                ? [255, 0, 0, 200]
-                : [0, 0, 0, 0];
-            }
+          //   if (dataView === 'watered' && communityData[id]) {
+          //     return communityData[id].watered
+          //       ? [0, 0, 255, 200]
+          //       : [0, 0, 0, 0];
+          //   }
 
-            if (dataView === 'adopted' || dataView === 'watered') {
-              return [0, 0, 0, 0];
-            }
+          //   if (dataView === 'adopted' && communityData[id]) {
+          //     return communityData[id].adopted
+          //       ? [255, 0, 0, 200]
+          //       : [0, 0, 0, 0];
+          //   }
 
-            if (age >= ageRange[0] && age <= ageRange[1]) {
-              const interpolated = interpolateColor(radolan_sum);
-              const hex = hexToRgb(interpolated);
+          //   if (dataView === 'adopted' || dataView === 'watered') {
+          //     return [0, 0, 0, 0];
+          //   }
 
-              return hex;
-            }
+          //   if (age >= ageRange[0] && age <= ageRange[1]) {
+          //     const interpolated = interpolateColor(radolan_sum);
+          //     const hex = hexToRgb(interpolated);
 
-            if (Number.isNaN(age)) {
-              // const interpolated = interpolateColor(radolan_sum);
-              // const hex = hexToRgb(interpolated);
-              return [200, 200, 200, 0];
-              // return hex;
-            }
+          //     return hex;
+          //   }
 
-            return [200, 200, 200, 0];
-          },
+          //   if (Number.isNaN(age)) {
+          //     // const interpolated = interpolateColor(radolan_sum);
+          //     // const hex = hexToRgb(interpolated);
+          //     return [200, 200, 200, 0];
+          //     // return hex;
+          //   }
+
+          //   return [200, 200, 200, 0];
+          // },
           onClick: info => {
             const { setDetailRouteWithListPath } = this.props;
             this._onClick(info.x, info.y, info.object);
