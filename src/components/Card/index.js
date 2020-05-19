a920140ca796f69a28275e16825b8349bdbff4a7
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-05-19 15:18:49 +0200

    housekeeping

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -76,13 +76,6 @@ const Card = p => {
     gattungdeutsch,
   } = data;
 
-  /**
-   * Apparently DWD 1 is not 1ml but 0.1ml
-   * We could change this in the database, but this would mean,
-   * transferring 625.000 "," characters, therefore,
-   * changing it client-side makes more sense.
-   */
-  const adjusted_radolan_sum = radolan_sum / 10;
   const getTreeProp = p => {
     return p === 'null' ? null : p;
   };
