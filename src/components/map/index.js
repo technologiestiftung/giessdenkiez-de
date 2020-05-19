540a4361a68ebb4c53fd6a9dee6e80a09bf3aa31
Sebastian Meier <contact@sebastianmeier.eu>
2020-05-19 14:11:01 +0200

    fix dwd scale (#140)

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -185,7 +185,13 @@ class DeckGLMap extends React.Component {
           wireframe: true,
           getElevation: 1,
           getFillColor: f => {
-            const interpolated = interpolateColor(f.properties.data[0]);
+            /**
+             * Apparently DWD 1 is not 1ml but 0.1ml
+             * We could change this in the database, but this would mean, 
+             * transferring 625.000 "," characters, therefore,
+             * changing it client-side makes more sense.
+             */
+            const interpolated = interpolateColor(f.properties.data[0]/10);
             const hex = hexToRgb(interpolated);
             return hex;
           },
