595dcfe55d24727fc979d8ceedda9c5bd915cedc
Daniel Molnar <dm@tpwd.de>
2020-06-29 22:44:31 +0200

    Fix typos in non-verified-mail.tsx
    - inserted commas
    - corrected Spam-Ordner

--- src/components/Card/non-verified-mail.tsx
+++ src/components/Card/non-verified-mail.tsx
@@ -5,9 +5,9 @@ export const NonVerfiedMailCardParagraph = () => {
   return (
     <CardParagraph>
       Du hast deine E-Mail Adresse noch nicht verifiziert. Bitte wirf einen
-      Blick in dein Postfach ob du eine Mail von
+      Blick in dein Postfach, ob du eine E-Mail von
       &ldquo;no-reply@auth0user.net&ldquo; erhalten hast und klicke auf den
-      enthaltenen Link um dies zu tun. Vielleicht sind wir auch im Spam Ordern
+      enthaltenen Link, um dies zu tun. Vielleicht sind wir auch im Spam-Ordner
       gelandet?
     </CardParagraph>
   );
