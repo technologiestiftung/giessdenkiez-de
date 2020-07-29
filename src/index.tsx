b8df4f226693cdc7c9ca22aaef9703f60ca5db69
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-07-29 09:15:34 +0200

    adds fake auth0 provider
    doing this during runtime is not a good solution. Should be done during build

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import 'react-app-polyfill/stable';
+import 'whatwg-fetch';
+import 'core-js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
@@ -7,11 +9,12 @@ import history from './history';
 import ErrorBoundary from './ErrorBoundary';
 
 import { Provider } from 'unistore/react';
-import Store from './state/Store';
+import store from './state/Store';
 import GlobalStyles from './assets/Global';
 
 import { Auth0Provider } from './utils/auth0';
-
+import { FakeAuth0Provider } from './dev-mode/fake-auth0';
+let devMode = false;
 const onRedirectCallback = appState => {
   history.push(
     appState && appState.targetUrl
@@ -19,26 +22,54 @@ const onRedirectCallback = appState => {
       : window.location.pathname
   );
 };
-function startApp() {
+function startApp(): void {
+  const url: URL = new URL(window.location.href);
+  const params: URLSearchParams = url.searchParams;
+
+  console.log(params);
+  if (params.has('mode')) {
+    const mode = params.get('mode');
+    if (mode === 'DEV_MODE') {
+      devMode = true;
+      console.log('WE ARE IN DEV_MODE!!!!!');
+    }
+  }
   const rootElement = document.getElementById('app');
 
-  function renderApp(RootComponent) {
+  function renderApp(RootComponent: () => JSX.Element): void {
     ReactDOM.render(
       <ErrorBoundary>
-        <Auth0Provider
-          domain={process.env.AUTH0_DOMAIN}
-          client_id={process.env.AUTH0_CLIENT_ID}
-          audience={process.env.AUTH0_AUDIENCE}
-          redirect_uri={window.location.origin}
-          onRedirectCallback={onRedirectCallback}
-        >
-          <Provider store={Store}>
-            <>
-              <GlobalStyles />
-              <RootComponent />
-            </>
-          </Provider>
-        </Auth0Provider>
+        {devMode === false ? (
+          <Auth0Provider
+            domain={process.env.AUTH0_DOMAIN}
+            client_id={process.env.AUTH0_CLIENT_ID}
+            audience={process.env.AUTH0_AUDIENCE}
+            redirect_uri={window.location.origin}
+            onRedirectCallback={onRedirectCallback}
+          >
+            <Provider store={store}>
+              <>
+                <GlobalStyles />
+                <RootComponent />
+              </>
+            </Provider>
+          </Auth0Provider>
+        ) : (
+          <FakeAuth0Provider
+            domain={process.env.AUTH0_DOMAIN}
+            client_id={process.env.AUTH0_CLIENT_ID}
+            audience={process.env.AUTH0_AUDIENCE}
+            redirect_uri={window.location.origin}
+            onRedirectCallback={onRedirectCallback}
+          >
+            <Provider store={store}>
+              <>
+                <GlobalStyles />
+                <RootComponent />
+              </>
+            </Provider>
+          </FakeAuth0Provider>
+        )}
       </ErrorBoundary>,
       rootElement
     );
