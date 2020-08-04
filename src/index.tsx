08751aae70e641f6d15197be9cc28c4ff0fa8ceb
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-08-04 16:34:43 +0200

    remove some unneeded nesting

--- src/index.tsx
+++ src/index.tsx
@@ -12,71 +12,41 @@ import { Provider } from 'unistore/react';
 import store from './state/Store';
 import GlobalStyles from './assets/Global';
 
-import { Auth0Provider } from './utils/auth0';
-import { FakeAuth0Provider } from './dev-mode/fake-auth0';
-let devMode = false;
-const onRedirectCallback = appState => {
+import { Auth0Provider } from './utils/auth/auth0';
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const onRedirectCallback: (appState: any) => void = appState => {
   history.push(
     appState && appState.targetUrl
       ? appState.targetUrl
       : window.location.pathname
   );
 };
-function startApp(): void {
-  const url: URL = new URL(window.location.href);
-  const params: URLSearchParams = url.searchParams;
 
-  console.log(params);
-  if (params.has('mode')) {
-    const mode = params.get('mode');
-    if (mode === 'DEV_MODE') {
-      devMode = true;
-      console.log('WE ARE IN DEV_MODE!!!!!');
-    }
-  }
-  const rootElement = document.getElementById('app');
+ReactDOM.render(
+  <ErrorBoundary>
+    <Auth0Provider
+      domain={process.env.AUTH0_DOMAIN}
+      client_id={process.env.AUTH0_CLIENT_ID}
+      audience={process.env.AUTH0_AUDIENCE}
+      redirect_uri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      <Provider store={store}>
+        <>
+          <GlobalStyles />
+          <Root />
+        </>
+      </Provider>
+    </Auth0Provider>
+  </ErrorBoundary>,
 
-  function renderApp(RootComponent: () => JSX.Element): void {
-    ReactDOM.render(
-      <ErrorBoundary>
-        {devMode === false ? (
-          <Auth0Provider
-            domain={process.env.AUTH0_DOMAIN}
-            client_id={process.env.AUTH0_CLIENT_ID}
-            audience={process.env.AUTH0_AUDIENCE}
-            redirect_uri={window.location.origin}
-            onRedirectCallback={onRedirectCallback}
-          >
-            <Provider store={store}>
-              <>
-                <GlobalStyles />
-                <RootComponent />
-              </>
-            </Provider>
-          </Auth0Provider>
-        ) : (
-          <FakeAuth0Provider
-            domain={process.env.AUTH0_DOMAIN}
-            client_id={process.env.AUTH0_CLIENT_ID}
-            audience={process.env.AUTH0_AUDIENCE}
-            redirect_uri={window.location.origin}
-            onRedirectCallback={onRedirectCallback}
-          >
-            <Provider store={store}>
-              <>
-                <GlobalStyles />
-                <RootComponent />
-              </>
-            </Provider>
-          </FakeAuth0Provider>
-        )}
-      </ErrorBoundary>,
-      rootElement
-    );
-  }
+  document.getElementById('app')
+);
 
-  // Mount the react-app
-  renderApp(Root);
-}
+// function renderApp(RootComponent: () => JSX.Element): void {}
 
-startApp();
+// Mount the react-app
+// renderApp(Root);
+// }
+
+// startApp();
