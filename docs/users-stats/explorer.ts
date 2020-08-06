cb34d0450cfe591fcd4299473f2d9de415f8b8f8
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-08-06 15:46:43 +0200

    prettier

--- docs/users-stats/explorer.ts
+++ docs/users-stats/explorer.ts
@@ -1,5 +1,5 @@
 //explorer.ts
-import { readFileStr, load } from "./mod.ts";
+import { readFileStr, load } from './mod.ts';
 
 async function main(): Promise<void> {
   await load();
@@ -7,19 +7,19 @@ async function main(): Promise<void> {
   const perpage = 50;
   let page = 0;
 
-  const token = await readFileStr("./token", { encoding: "utf8" });
+  const token = await readFileStr('./token', { encoding: 'utf8' });
   let next = true;
 
   const users = [];
 
   const headers = new Headers({
     Authorization: `Bearer ${token}`,
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   });
   while (next) {
     const url = `${apiurl}/users?page=${page}&per_page=${perpage}`;
     const res = await fetch(url, {
-      method: "GET",
+      method: 'GET',
       headers,
     });
     if (!res.ok) {
@@ -35,23 +35,23 @@ async function main(): Promise<void> {
 
   const domains: string[] = [];
   const notVerified = [];
-  users.forEach((user) => {
+  users.forEach(user => {
     if (user.email_verified === false) {
       // console.log(user.email);
-      const domain = user.email.split("@")[1];
+      const domain = user.email.split('@')[1];
 
       domains.push(domain);
       notVerified.push(user);
     }
   });
-  const mails = users.map((user) => user.email);
+  const mails = users.map(user => user.email);
   const uniqueDomains = [...new Set(domains)];
 
-  console.log("-----------users-----------");
-  console.log("users tried to signed up          ", users.length);
-  console.log("----- email verification -------");
-  console.log("number not verfied user mails     ", notVerified.length);
-  console.log("unique not verified domains       ", uniqueDomains.join(", "));
+  console.log('-----------users-----------');
+  console.log('users tried to signed up          ', users.length);
+  console.log('----- email verification -------');
+  console.log('number not verfied user mails     ', notVerified.length);
+  console.log('unique not verified domains       ', uniqueDomains.join(', '));
 
   // const diskjson = (await readJson("./giessdenkiez-users.json")) as any[];
 
