b5341be696f72e8a72aa3d6994f81d5d55e59c29
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-09-02 14:05:39 +0200

    styling hover object

--- src/components/map/HoverObject.tsx
+++ src/components/map/HoverObject.tsx
@@ -14,31 +14,35 @@ export interface StyledProps {
 const StyledSpan = styled.span`
   padding: 1rem 1rem 0.2rem 1rem;
   margin: 2rem auto;
+  color: ${props => props.theme.colorTextLight};
 `;
 const Bubble = styled.div<StyledProps>`
-  height: 23px;
+  height: 30px;
+  line-height: 30px;
+  font-size: ${props => props.theme.fontSizeL};
+  box-sizing: border-box;
   box-shadow: rgba(51, 51, 102, 0.5) 2px 2px 2px;
+  font-family: ${props => props.theme.fontFamily};
   position: absolute;
   z-index: 10;
   pointer-events: none;
   display: inline-block;
   text-align: center;
   vertical-align: middle;
-  left: ${props => props.pointer[0] - props.width / 2}px;
-  top: ${props => props.pointer[1] - props.height * 2}px;
+  left: ${props => props.pointer[0] - props.width / 2 - 3}px;
+  top: ${props => props.pointer[1] - 45}px;
   background-color: #ffffff;
   margin: 0 auto;
-
   &::after {
     -moz-transform: rotate(45deg);
     -webkit-transform: rotate(45deg);
     border-width: 7px;
     border-style: solid;
     border-color: transparent #fff #fff transparent;
-    bottom: -6.5px;
     content: '';
     position: absolute;
-    left: ${props => props.pointer[0] - props.width / 4}px;
+    bottom: -6px;
+    left: 48%;
     box-shadow: rgba(51, 51, 102, 0.5) 2px 2px 2px;
   }
 `;
@@ -69,7 +73,9 @@ export const HoverObject: React.FC<HoverObjectProps> = ({
           width={widthBubble}
           height={heightBubble}
         >
-          <StyledSpan>Status: {message}</StyledSpan>
+          <StyledSpan>
+            <b>Status:</b> {message}
+          </StyledSpan>
         </Bubble>
       </div>
     </>
