d4464daa0aa42bff70dff7fb734227013c1e8e94
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-02-16 13:07:00 +0100

    fix: Reverts changes made to merge inport
    Rolled back to webpack 4x

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { merge } = require('webpack-merge');
+const merge = require('webpack-merge');
 const common = require('./common.config');
 const webpack = require('webpack'); // to access built-in plugins
 
