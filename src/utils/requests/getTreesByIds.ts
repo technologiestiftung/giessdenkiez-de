c220dc0ab6ba33116d2c5cadf5bfafcd70133eb2
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-03-23 18:20:03 +0100

    fix(request): Request does not need the curly braces

--- src/utils/requests/getTreesByIds.ts
+++ src/utils/requests/getTreesByIds.ts
@@ -5,8 +5,8 @@ import { requests } from '../requestUtil';
 export const getTreesByIds = async (ids: string[]): Promise<Tree[]> => {
   const queryStr = ids.reduce(
     (acc: string, curr: string, idx: number, arr: string[]) =>
-      idx + 1 === arr.length ? `${acc}${curr}}` : `${acc}${curr},`,
-    '{'
+      idx + 1 === arr.length ? `${acc}${curr}` : `${acc}${curr},`,
+    ''
   );
 
   const url = createAPIUrl(`/get/?queryType=treesbyids&tree_ids=${queryStr}`);
