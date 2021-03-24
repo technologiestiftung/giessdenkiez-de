11ac9a447729e999f6f0a5a62ff65b16a12045b2
Lucas Vogel <contact@vogelino.com>
2021-03-24 22:44:22 +0100

    refactor(UsersWateringsList): Add abb. to "show more" waterings if needed

--- src/components/UsersWateringsList/index.tsx
+++ src/components/UsersWateringsList/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styled from 'styled-components';
 import { WateringType } from '../../common/interfaces';
 
@@ -45,13 +45,45 @@ const Title = styled.span`
   font-size: ${p => p.theme.fontSizeL};
 `;
 
+const ToggleExpansionLink = styled.button`
+  border: 0;
+  background: none;
+  margin: 0;
+  padding: 0;
+  text-align: left;
+  font-family: inherit;
+  font-size: ${p => p.theme.fontSizeL};
+  color: ${p => p.theme.colorTextDark};
+  text-decoration: underline;
+  cursor: pointer;
+  text-align: center;
+  padding: 8px;
+  transition: color 200ms ease-out;
+  outline: none;
+
+  &:hover {
+    color: ${p => p.theme.colorTextLight};
+  }
+
+  &:focus {
+    border-radius: 3px;
+    box-shadow: 0 0 0 2px ${p => p.theme.colorTextLight};
+  }
+`;
+
+const MAX_ITEMS = 8;
+
 const UsersWateringsList: FC<{
   waterings: WateringType[];
-}> = ({ waterings }) => (
-  <WrapperOuter>
-    {waterings.map(
-      ({ username, timestamp, amount }: WateringType, i: number) => {
-        return (
+}> = ({ waterings }) => {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+  const surpassedMaxItems = waterings.length > MAX_ITEMS;
+  const listItems = isExpanded ? waterings : waterings.slice(0, MAX_ITEMS);
+
+  return (
+    <WrapperOuter>
+      {listItems.map(
+        ({ username, timestamp, amount }: WateringType, i: number) => (
           <Wrapper key={`Lastadopted-key-${i}`}>
             <FlexRow>
               <Title>{username}</Title>
@@ -62,10 +94,19 @@ const UsersWateringsList: FC<{
             <SmallParagraph>{amount}l</SmallParagraph>
             <StyledIcon src={iconDrop} alt='Water drop icon' />
           </Wrapper>
-        );
-      }
-    )}
-  </WrapperOuter>
-);
+        )
+      )}
+      {surpassedMaxItems && (
+        <ToggleExpansionLink onClick={() => setIsExpanded(!isExpanded)}>
+          {isExpanded
+            ? 'Weniger anzeigen'
+            : `${
+                waterings.length - MAX_ITEMS
+              } zusätzliche Bewässerungen anzeigen`}
+        </ToggleExpansionLink>
+      )}
+    </WrapperOuter>
+  );
+};
 
 export default UsersWateringsList;
