2353b258169ef054f3563bb1bdc0b1b01f33e66f
Lucas Vogel <contact@vogelino.com>
2021-03-24 20:14:43 +0100

    refactor(selectedTreeLastWatered): Remove selectedTreeLastWatered from store state

--- src/state/Store.tsx
+++ src/state/Store.tsx
@@ -17,7 +17,6 @@ const initialState = {
   ageRange: [0, 320],
   pumps: null,
   data: null,
-  selectedTreeLastWatered: undefined,
   overlay: true,
   isTreeDataLoading: true,
   hoveredObject: false,
