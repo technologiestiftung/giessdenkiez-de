f6618868361ffa9dba9d9b3d80b8c60ba6968090
Lucas Vogel <contact@vogelino.com>
2021-03-24 09:13:08 +0100

    refactor(Sidebar): remove unecessary withROuter HOC

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Route, withRouter, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import SidebarAbout from './SidebarAbout/';
 import SidebarSearch from './SidebarSearch/';
@@ -62,4 +62,4 @@ const Sidebar: React.FC = () => (
   />
 );
 
-export default withRouter(Sidebar);
+export default Sidebar;
