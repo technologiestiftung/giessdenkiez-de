73347bc6d2145bab32a961995b005421e106693a
Lucas Vogel <contact@vogelino.com>
2021-03-31 10:43:50 +0200

    test(ExpandablePanel): Fix tests for ExpandablePanel

--- src/components/ExpandablePanel/ExpandablePanel.test.tsx
+++ src/components/ExpandablePanel/ExpandablePanel.test.tsx
@@ -1,30 +1,32 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import ExpandablePanel from '.';
 
 describe('component ExpandablePanel', () => {
   test('should render its title', () => {
-    const { getByText } = render(
-      <ExpandablePanel title={'foo'}>some longer text</ExpandablePanel>
-    );
-    const title = getByText(/foo/i);
+    render(<ExpandablePanel title={'foo'}>some longer text</ExpandablePanel>);
+    const title = screen.getByText(/foo/i);
     expect(title).toBeInTheDocument();
   });
 
-  test('should render its children', () => {
-    const { getByText } = render(
-      <ExpandablePanel title={'foo'}>some longer text</ExpandablePanel>
-    );
-    const longText = getByText(/some longer text/i);
+  test('should not render its children if closed', () => {
+    render(<ExpandablePanel title={'foo'}>some longer text</ExpandablePanel>);
+    const longTexts = screen.queryByText(/some longer text/i);
+    expect(longTexts).not.toBeInTheDocument();
+  });
+
+  test('should render its children if opened', () => {
+    render(<ExpandablePanel title={'foo'}>some longer text</ExpandablePanel>);
+    const title = screen.getByText(/foo/i);
+    fireEvent.click(title);
+    const longText = screen.getByText(/some longer text/i);
     expect(longText).toBeInTheDocument();
   });
 
   test('should toggle indicator when indicator or title clicked', () => {
-    const { getByText } = render(
-      <ExpandablePanel title={'foo'}>some longer text</ExpandablePanel>
-    );
-    const indicator = getByText(/\+/i);
-    const title = getByText(/foo/i);
+    render(<ExpandablePanel title={'foo'}>some longer text</ExpandablePanel>);
+    const indicator = screen.getByText(/\+/i);
+    const title = screen.getByText(/foo/i);
 
     expect(indicator).toBeInTheDocument();
     // click once to open
