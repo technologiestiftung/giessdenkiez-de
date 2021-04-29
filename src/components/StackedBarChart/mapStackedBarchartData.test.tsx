6a0ae5bb5d51e3ec1e1140999d60170898ec0ce9
Lucas Soeth <lucasoeth@gmail.com>
2021-04-29 12:21:33 +0200

    test: extensive testing

--- src/components/StackedBarChart/mapStackedBarchartData.test.tsx
+++ src/components/StackedBarChart/mapStackedBarchartData.test.tsx
@@ -9,7 +9,8 @@ const waterings = arrayOf30.map(val => ({
   timestamp: new Date().toISOString(),
   username: 'vogelino',
 }));
-const selectedTree = {
+
+const testSelectedTree = {
   // eslint-disable-next-line
   radolan_days: [0,0,0,0,0,0,0,0,0,0,0,0,2.8,0.1,2.2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2.4,0.1,0,0.3,0,0,0,0,0,0.6,0.3,2.4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.2,0.1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.2,0,0.6,0.5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.2,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.3,0,0.1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.4,2.6,4.7,0,0.2,0,0.5,0,0,0,0,0,0,0,0,0,0,0,0,0.3,1.4,0.4,0,0,0,0,6.6,0,0,0.1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.1,1.1,0.5,0.9,0.8,1.3,1.2,0,0,0,0,1.4,0,0,0,0,0,0,0,0.4,0,0,0,0,0.4,0,0.4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1.6,0.2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2.2,0.1,0.2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.5,0.1,0.1,0,0,3.6,3.6,0,0,0.5,0.8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.3,0.1,0.2,0,0,0,0,0],
   radolan_sum: 10,
@@ -22,8 +23,19 @@ const selectedTree = {
 
 describe('mapStackedBarchartData', () => {
   test('should combine waterings and selectedTree data', () => {
-    const mappedStackedBarchartData = mapStackedBarchartData(selectedTree);
+    const mappedStackedBarchartData = mapStackedBarchartData(testSelectedTree);
     expect(mappedStackedBarchartData[0].rainValue).toBe(0);
     expect(mappedStackedBarchartData[1].rainValue).toBe(0.6000000000000001);
+  });
+  test('should have length 30', () => {
+    const mappedStackedBarchartData = mapStackedBarchartData(testSelectedTree);
+    expect(mappedStackedBarchartData).toHaveLength(30);
+  });
+  test('should have last 30 days', () => {
+    const mappedStackedBarchartData = mapStackedBarchartData(testSelectedTree);
+    const date = new Date(new Date().toISOString().split('T')[0]);
+    expect(mappedStackedBarchartData[0].id).toBe(+date);
+    date.setDate(date.getDate() - 29);
+    expect(mappedStackedBarchartData[29].id).toBe(+date);
   });
 });
