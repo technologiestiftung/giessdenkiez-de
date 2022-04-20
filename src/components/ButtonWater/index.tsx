59ac8f72d240e0fa793e0ad1f1c303a7ebdfb712
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-04-20 17:44:12 +0200

    feat(GDK-24): use new watering modal

--- src/components/ButtonWater/index.tsx
+++ src/components/ButtonWater/index.tsx
@@ -1,101 +1,30 @@
 import React, { FC, useState } from 'react';
-import styled from 'styled-components';
 import { useCurrentTreeId } from '../../utils/hooks/useCurrentTreeId';
-import { useWateringActions } from '../../utils/hooks/useWateringActions';
 
 import ButtonRound from '../ButtonRound';
-import { NumberInput } from '../NumberInput';
 import { ParticipateButton } from '../ParticipateButton';
-
-import { buttonLabels } from './button-water-label-maker';
-
-const BtnWaterContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
-  margin-top: 10px;
-  width: 100%;
-`;
-
-const BtnWaterSuggestions = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-`;
-
-const CustomWateringContainer = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: 96px auto;
-  align-items: flex-end;
-  gap: 8px;
-  margin: 10px 0;
-`;
+import { WateringModal } from '../WateringModal';
 
 const ButtonWater: FC = () => {
   const treeId = useCurrentTreeId();
-  const { isBeingWatered, waterTree } = useWateringActions(treeId);
-  const [waterButtonIsOpened, setWaterButtonIsOpened] = useState<boolean>(
-    false
-  );
-  const [customWateringValue, setCustomWateringValue] = useState(0);
+  const [wateringIsInitiated, setWateringIsInitiated] = useState(false);
 
   if (!treeId) return null;
   return (
     <>
+      <WateringModal
+        isOpen={wateringIsInitiated}
+        setIsOpen={setWateringIsInitiated}
+      />
       <ButtonRound
         width='-webkit-fill-available'
-        onClick={() => setWaterButtonIsOpened(!waterButtonIsOpened)}
+        onClick={() => {
+          setWateringIsInitiated(true);
+        }}
         type='primary'
       >
-        {waterButtonIsOpened &&
-          isBeingWatered &&
-          'Bewässerung wird eingetragen'}
-        {waterButtonIsOpened && !isBeingWatered && 'Wieviel Wasser?'}
-        {!waterButtonIsOpened && 'Ich habe gegossen!'}
+        Ich habe gegossen!
       </ButtonRound>
-      {waterButtonIsOpened && (
-        <BtnWaterContainer>
-          <BtnWaterSuggestions>
-            {buttonLabels.map(btn => {
-              return (
-                <ButtonRound
-                  key={`${btn.amount}`}
-                  width='fit-content'
-                  onClick={() =>
-                    waterTree(btn.amount).finally(() =>
-                      setWaterButtonIsOpened(false)
-                    )
-                  }
-                  type='primary'
-                >
-                  {btn.label}
-                </ButtonRound>
-              );
-            })}
-          </BtnWaterSuggestions>
-          <CustomWateringContainer>
-            <NumberInput
-              id='custom-watering-value'
-              label='Andere Literanzahl'
-              onChange={event => {
-                setCustomWateringValue(Number(event.target.value));
-              }}
-            />
-            <ButtonRound
-              key={`submit-custom-watering`}
-              width='fit-content'
-              onClick={() =>
-                waterTree(customWateringValue).finally(() =>
-                  setWaterButtonIsOpened(false)
-                )
-              }
-              type='primary'
-            >
-              Senden
-            </ButtonRound>
-          </CustomWateringContainer>
-        </BtnWaterContainer>
-      )}
       <ParticipateButton />
     </>
   );
