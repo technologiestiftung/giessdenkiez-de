7c4e96a39b3313ec321def6dac1beec381271f8d
Lucas Vogel <contact@vogelino.com>
2022-11-07 16:52:35 +0100

    chore: Remove unused import

--- src/components/MapLayout.tsx
+++ src/components/MapLayout.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode } from 'react';
+import React, { FC } from 'react';
 import Head from 'next/head';
 import App from './App';
 import SidebarWrapper from './Sidebar/SidbarWrapper';
