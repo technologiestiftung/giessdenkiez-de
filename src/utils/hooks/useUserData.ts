1f6c7225fe5e9887c8e6c1513bf1a5644f62fd35
ff6347 <hash@ff6347.email>
2023-03-21 17:36:01 +0100

    fix: Error paste sql function

--- src/utils/hooks/useUserData.ts
+++ src/utils/hooks/useUserData.ts
@@ -40,31 +40,7 @@ export const useUserData = (): {
 
   let userData: UserDataType | undefined;
   if (user && partialUserData && profile) {
-{
-
-Create a trigger function that updates the table trees_watered whenever the table column username on the table
-profiles changes.
-
-CREATE TABLE "public"."profiles" (
-    "id" uuid NOT NULL,
-    "username" text
-);
--- Sequence and defined type
-CREATE SEQUENCE IF NOT EXISTS trees_watered_id_seq;
-
--- Table Definition
-CREATE TABLE "public"."trees_watered" (
-    "time" text,
-    "uuid" text,
-    "amount" numeric NOT NULL CHECK (amount > (0)::numeric),
-    "timestamp" timestamptz NOT NULL,
-    "username" text,
-    "id" int4 NOT NULL DEFAULT nextval('trees_watered_id_seq'::regclass),
-    "tree_id" text NOT NULL,
-    CONSTRAINT "fk_trees_watered_trees" FOREIGN KEY ("tree_id") REFERENCES "public"."trees"("id"),
-    PRIMARY KEY ("id")
-);
-}    userData = {
+    userData = {
       id: user.id,
       email: user.email!,
       isVerified: user.email_confirmed_at !== undefined ? true : false,
