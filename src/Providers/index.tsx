9729987e5211f0d40e21651bf5642cbaf8bc0860
ff6347 <hash@ff6347.email>
2023-03-22 16:55:57 +0100

    refactor: Remove auth0 context

--- src/Providers/index.tsx
+++ src/Providers/index.tsx
@@ -4,7 +4,6 @@ import { ReactQueryDevtools } from 'react-query/devtools';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'unistore/react';
 
-import { Auth0Provider } from '@auth0/auth0-react';
 import GlobalStyles from '../assets/Global';
 import { theme } from '../assets/theme';
 import ErrorBoundary from '../ErrorBoundary';
@@ -36,24 +35,15 @@ export const Providers: FC = ({ children }) => {
         supabaseClient={supabaseClient}
         initialSession={session}
       >
-        <Auth0Provider
-          domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN || ''}
-          clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || ''}
-          audience={process.env.NEXT_PUBLIC_AUTH0_AUDIENCE || ''}
-          redirectUri={`${process.env.NEXT_PUBLIC_BASE_URL}/profile`}
-          useRefreshTokens
-          cacheLocation='localstorage'
-        >
-          <QueryClientProvider client={queryClient}>
-            <ReactQueryDevtools initialIsOpen={false} />
-            <Provider store={store}>
-              <ThemeProvider theme={theme}>
-                <GlobalStyles />
-                {children}
-              </ThemeProvider>
-            </Provider>
-          </QueryClientProvider>
-        </Auth0Provider>
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools initialIsOpen={false} />
+          <Provider store={store}>
+            <ThemeProvider theme={theme}>
+              <GlobalStyles />
+              {children}
+            </ThemeProvider>
+          </Provider>
+        </QueryClientProvider>
       </SessionContextProvider>
     </ErrorBoundary>
   );
