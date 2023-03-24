758388a05ae18818d949db22fbff4cbcba6b81c8
ff6347 <hash@ff6347.email>
2023-03-24 13:36:20 +0100

    feat(Notification):Extend Notification w meta data

--- src/components/Sidebar/SidebarAuth/Notification.tsx
+++ src/components/Sidebar/SidebarAuth/Notification.tsx
@@ -1,9 +1,56 @@
 import React from 'react';
 import styled from 'styled-components';
 
+function uuidv4(): string {
+  const cryptoObj = window.crypto || window['msCrypto'];
+  if (!cryptoObj) {
+    return '';
+  }
+
+  const bytes = new Uint8Array(16);
+  cryptoObj.getRandomValues(bytes);
+
+  bytes[6] = (bytes[6] & 0x0f) | 0x40;
+  bytes[8] = (bytes[8] & 0x3f) | 0x80;
+
+  const hex = Array.prototype.map
+    .call(bytes, (x: number) => ('00' + x.toString(16)).slice(-2))
+    .join('');
+
+  return (
+    hex.substr(0, 8) +
+    '-' +
+    hex.substr(8, 4) +
+    '-4' +
+    hex.substr(12, 3) +
+    '-' +
+    hex.substr(16, 4) +
+    '-' +
+    hex.substr(20)
+  );
+}
+
+export function createUserNotifiction({
+  message,
+  type,
+  dispatchedFrom,
+}: {
+  message: string;
+  type: UserNotificationType;
+  dispatchedFrom?: string;
+}): UserNotificationObjectType {
+  return {
+    id: uuidv4(),
+    message,
+    type,
+    dispatchedFrom,
+  };
+}
 export type UserNotificationObjectType = {
   message: string;
   type: UserNotificationType;
+  id?: string;
+  dispatchedFrom?: string;
 };
 export type UserNotificationType = 'success' | 'error';
 
