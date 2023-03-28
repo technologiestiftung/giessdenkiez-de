f1e8f7a224274e830bad3a22fef4e21c77bb6908
ff6347 <hash@ff6347.email>
2023-03-28 16:06:05 +0200

    refactor(passowrd): Move edits to profile

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,17 +3,18 @@ import { MapLayout } from '../src/components/MapLayout';
 import { Page } from '../src/nextPage';
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 import { SidebarAuth } from '../src/components/Sidebar/SidebarAuth';
-import { PasswordResetForm } from '../src/components/Sidebar/SidebarAuth/PasswordResetForm';
-import ButtonRound from '../src/components/ButtonRound';
 import { StyledFlexContainer, StyledFormRow } from '../src/components/Forms';
 import {
   UserNotification,
   UserNotificationObjectType,
-} from '../src/components/Sidebar/SidebarAuth/Notification';
-import { PasswordRecoveryForm } from '../src/components/Sidebar/SidebarAuth/PasswordRecoveryForm';
+} from '../src/components/Notification';
+
 import { SignOut } from '../src/components/Sidebar/SidebarAuth/SignOut';
 import SidebarTitle from '../src/components/Sidebar/SidebarTitle';
 import Router from 'next/router';
+import SmallParagraph from '../src/components/SmallParagraph';
+import Link from 'next/link';
+import { PasswordRecoveryForm } from '../src/components/Forms/PasswordRecoveryForm';
 export type AuthView = 'signin' | 'signup' | 'recovery' | 'confirm';
 
 const AuthPage: Page = () => {
@@ -22,7 +23,6 @@ const AuthPage: Page = () => {
   const [showPasswordRecoveryScreen, setShowPasswordRecoveryScreen] = useState(
     false
   );
-  const [showPasswordChangeForm, setShowPasswordChangeForm] = useState(false);
   const [view, setView] = useState<AuthView>('signin');
 
   const [
@@ -39,32 +39,12 @@ const AuthPage: Page = () => {
   useEffect(() => {
     const {
       data: { subscription: authListener },
-    } = supabase.auth.onAuthStateChange((event, session) => {
+    } = supabase.auth.onAuthStateChange(event => {
       switch (event) {
-        case 'SIGNED_IN':
-          // console.log('SIGNED_IN', session);
-          break;
-        case 'SIGNED_OUT':
-          // console.log('SIGNED_OUT');
-          break;
-        case 'USER_UPDATED':
-          // console.log('USER_UPDATED', session?.user);
-          break;
         case 'PASSWORD_RECOVERY':
-          // console.log('PASSWORD_RECOVERY', session);
-
           // show screen to update user's password
           setShowPasswordRecoveryScreen(true);
           break;
-        case 'TOKEN_REFRESHED':
-          // console.log('TOKEN_REFRESHED', session);
-          break;
-        case 'USER_DELETED':
-          // console.log('USER_DELETED');
-          break;
-        default:
-          // console.log('UNKNOWN_EVENT', event);
-          throw new Error('Unknown event type');
       }
     });
 
@@ -102,32 +82,6 @@ const AuthPage: Page = () => {
     );
   }
 
-  if (showPasswordChangeForm) {
-    return (
-      <>
-        <PasswordResetForm
-          setNotification={setCurrentNotification}
-          returnClickHandler={() => {
-            setShowPasswordChangeForm(false);
-            setView('signin');
-          }}
-          additionalSubmitHandler={() => {
-            setShowPasswordChangeForm(false);
-          }}
-        />
-        <StyledFlexContainer>
-          <StyledFormRow>
-            {currentNotification && (
-              <UserNotification
-                type={currentNotification.type}
-                message={currentNotification.message}
-              />
-            )}
-          </StyledFormRow>
-        </StyledFlexContainer>
-      </>
-    );
-  }
   return (
     <div
       style={{
@@ -143,40 +97,16 @@ const AuthPage: Page = () => {
         />
       ) : (
         <>
-          {/* <UpdateUserDataForm setNotification={setCurrentNotification} /> */}
           <SidebarTitle>{'Auth'}</SidebarTitle>
-
+          <SmallParagraph>
+            Ups. Du solltest hier gar nicht sein. Wie wäre es mit deinem{' '}
+            <Link href={'/profile'}>Profil?</Link>
+          </SmallParagraph>
           <StyledFlexContainer>
             <StyledFormRow>
               <SignOut setView={setView} />
             </StyledFormRow>
           </StyledFlexContainer>
-          {/* <ExpandablePanel isExpanded={false} title={'Passwort'}>
-              <StyledFormRow>
-                <ButtonRound
-                  onClick={e => {
-                    e?.preventDefault();
-
-                    setShowPasswordRecoveryScreen(true);
-                  }}
-                >
-                  Passwort ändern?
-                </ButtonRound>
-              </StyledFormRow>
-              <StyledFormRow>
-                {process.env.NODE_ENV !== 'production' && (
-                  <details>
-                    <summary>Dev Info: Session</summary>
-                    <pre
-                      style={{ wordWrap: 'break-word', whiteSpace: 'pre-wrap' }}
-                    >
-                      <code>{JSON.stringify(session, null, 2)}</code>
-                    </pre>
-                  </details>
-                )}
-              </StyledFormRow>
-            </StyledFlexContainer>
-          </ExpandablePanel> */}
         </>
       )}
       <StyledFlexContainer>
