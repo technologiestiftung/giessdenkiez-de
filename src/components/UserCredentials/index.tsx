92b7e99acad043b0ca8e086f4005878e0960f308
ff6347 <hash@ff6347.email>
2023-03-28 16:05:07 +0200

    feat(password edit): Allow password edit from profile

--- src/components/UserCredentials/index.tsx
+++ src/components/UserCredentials/index.tsx
@@ -1,12 +1,12 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styled from 'styled-components';
-import { AccountEditModal } from '../AccountEditModal';
+import { AccountEditModal, ModalViewTypes } from '../AccountEditModal';
 import ExpandablePanel from '../ExpandablePanel';
 import { StyledA } from '../Forms';
-import {} from '../Sidebar/SidebarAuth/Notification';
+import {} from '../Notification';
 import SmallParagraph from '../SmallParagraph';
 
-const CredentialValue = styled.p`
+export const CredentialValue = styled.p`
   display: block;
   font-size: ${p => p.theme.fontSizeL};
   margin: 0 0 4px;
@@ -16,26 +16,42 @@ const CardCredentials: FC<{
   username: string;
   email: string;
 }> = ({ username, email }) => {
-  const [isBeingEdited, setIsBeingEdited] = React.useState(false);
+  const [isBeingEdited, setIsBeingEdited] = useState(false);
+  const [modalView, setModalView] = useState<ModalViewTypes>('account');
   return (
     <>
       <AccountEditModal
         isOpen={isBeingEdited}
         setIsOpen={setIsBeingEdited}
+        view={modalView}
       ></AccountEditModal>
       <ExpandablePanel isExpanded title='Dein Account'>
+        <SmallParagraph>Benutzername</SmallParagraph>
         <CredentialValue>{username}</CredentialValue>
-        <CredentialValue>{email}</CredentialValue>
         <SmallParagraph>Registrierte E-Mail Adresse</SmallParagraph>
+        <CredentialValue>{email}</CredentialValue>
         <SmallParagraph>
-          Account{' '}
+          Benutzername oder E-Mail{' '}
           <StyledA
             onClick={e => {
               e.preventDefault();
+              setModalView('account');
               setIsBeingEdited(true);
             }}
           >
             bearbeiten?
+          </StyledA>
+        </SmallParagraph>
+        <SmallParagraph>
+          Passwort{' '}
+          <StyledA
+            onClick={e => {
+              e.preventDefault();
+              setModalView('password');
+              setIsBeingEdited(true);
+            }}
+          >
+            ändern?
           </StyledA>
         </SmallParagraph>
       </ExpandablePanel>
