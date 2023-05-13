744f0135bf5536a5925f6523ac8360791ed319f3
Philip Mattha <philip@mattha.net>
2023-05-13 19:05:14 +0200

    Add touch and other icons

--- src/components/MapLayout.tsx
+++ src/components/MapLayout.tsx
@@ -17,6 +17,33 @@ export const MapLayout: FC<{ treeId?: string | null }> = ({
           name='viewport'
           content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'
         />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/images/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/images/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/images/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/images/site.webmanifest' />
+        <link
+          rel='mask-icon'
+          href='/images/safari-pinned-tab.svg'
+          color='#5bbad5'
+        />
+        <link rel='shortcut icon' href='/images/favicon.ico' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='msapplication-config' content='/images/browserconfig.xml' />
+        <meta name='theme-color' content='#ffffff' />
         <link rel='icon' type='image/x-icon' href='/images/favicon.ico' />
         <meta name='title' content='Gieß den Kiez | CityLAB Berlin' />
         <meta
