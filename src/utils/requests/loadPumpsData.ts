f32460149e4ac4daebdf213eddfcfae0acddb818
Jens Winter-Hübenthal <jens.winter-huebenthal@bridgefield.de>
2023-06-26 21:21:55 +0200

    Fetch data assets from Supabase storage

--- src/utils/requests/loadPumpsData.ts
+++ src/utils/requests/loadPumpsData.ts
@@ -1,8 +1,5 @@
 import { ExtendedFeatureCollection } from 'd3-geo';
 import { requests } from '../requestUtil';
 
-export const loadPumpsData = async (): Promise<ExtendedFeatureCollection> => {
-  return await requests<ExtendedFeatureCollection>(
-    '/data/pumps.geojson.min.json'
-  );
-};
+export const loadPumpsData = async (): Promise<ExtendedFeatureCollection> =>
+    await requests<ExtendedFeatureCollection>(process.env.NEXT_PUBLIC_PUMPS_DATA_URL);
--- src/utils/requests/loadRainGeoJson.ts
+++ src/utils/requests/loadRainGeoJson.ts
@@ -1,9 +1,5 @@
 import { ExtendedFeatureCollection } from 'd3-geo';
 import { requests } from '../requestUtil';
 
-export const loadRainGeoJson = async (): Promise<ExtendedFeatureCollection> => {
-  const dataUrl =
-    process.env.NEXT_PUBLIC_RAIN_DATA_URL || 'https://tsb-trees.s3.eu-central-1.amazonaws.com/weather_light.geojson.gz';
-
-  return await requests<ExtendedFeatureCollection>(dataUrl);
-};
+export const loadRainGeoJson = async (): Promise<ExtendedFeatureCollection> =>
+    await requests<ExtendedFeatureCollection>(process.env.NEXT_PUBLIC_RAIN_DATA_URL);
