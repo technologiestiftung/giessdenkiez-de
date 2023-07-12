8a5fe4160250dbe94dc40cf435446af957f06097
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2023-07-12 17:04:17 +0200

    feat(rain): fetch from Supabase
    BREAKING CHANGES: Fetch weather data from Supabase instead of S3. Change rain workflows to use latest dwd harvester action.

--- src/utils/requests/loadRainGeoJson.ts
+++ src/utils/requests/loadRainGeoJson.ts
@@ -2,8 +2,5 @@ import { ExtendedFeatureCollection } from 'd3-geo';
 import { requests } from '../requestUtil';
 
 export const loadRainGeoJson = async (): Promise<ExtendedFeatureCollection> => {
-  const dataUrl =
-    process.env.NEXT_PUBLIC_RAIN_DATA_URL || 'https://tsb-trees.s3.eu-central-1.amazonaws.com/weather_light.geojson.gz';
-
-  return await requests<ExtendedFeatureCollection>(dataUrl);
+  return await requests<ExtendedFeatureCollection>(process.env.NEXT_PUBLIC_RAIN_DATA_URL!);
 };
