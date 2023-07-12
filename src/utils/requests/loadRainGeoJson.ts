c68c9b65426dc4f3cac29d24a6673f5d04388307
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2023-07-12 17:36:40 +0200

    Throw error when NEXT_PUBLIC_PUMPS_DATA_URL or NEXT_PUBLIC_RAIN_DATA_URL are not set

--- src/utils/requests/loadRainGeoJson.ts
+++ src/utils/requests/loadRainGeoJson.ts
@@ -2,5 +2,6 @@ import { ExtendedFeatureCollection } from 'd3-geo';
 import { requests } from '../requestUtil';
 
 export const loadRainGeoJson = async (): Promise<ExtendedFeatureCollection> => {
-  return await requests<ExtendedFeatureCollection>(process.env.NEXT_PUBLIC_RAIN_DATA_URL!);
+  if(!process.env.NEXT_PUBLIC_RAIN_DATA_URL) throw new Error("env var NEXT_PUBLIC_RAIN_DATA_URL should be defiend in your environment");
+  return await requests<ExtendedFeatureCollection>(process.env.NEXT_PUBLIC_RAIN_DATA_URL);
 };
