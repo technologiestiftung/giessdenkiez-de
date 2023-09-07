96e3ab37c8e084b59c7b1f025415ac2ff7f37786
ff6347 <hash@ff6347.email>
2023-09-07 10:05:48 +0200

    fix(osm): Fix link to mapcomplete tool
    We are linking to a version of the theme in our account
    and we are using a raw github url with a git hash now

--- src/components/TreesMap/osmUtil.ts
+++ src/components/TreesMap/osmUtil.ts
@@ -1,9 +1,9 @@
 export const getOSMEditorURL = (nodeId: number): string => {
-  const mapcompleteUrl = 'https://mapcomplete.osm.be/theme';
+  const mapcompleteUrl = 'https://mapcomplete.org';
   const params = new URLSearchParams();
   params.set(
     'userlayout',
-    'https://tordans.github.io/MapComplete-ThemeHelper/OSM-Berlin-Themes/man_made-walter_well-status-checker/theme.json'
+    'https://raw.githubusercontent.com/technologiestiftung/MapComplete-ThemeHelper/024ef9134987e0ead34f261a6270d5572c4d31a4/OSM-Berlin-Themes/man_made-walter_well-status-checker/theme.json'
   );
   params.set('language', 'de');
   const selectedPump = `#node/${nodeId}`;
