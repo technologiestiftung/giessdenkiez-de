02e2fc9ee0f219d1c0b7606563907dc29aed77c3
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-01-03 14:01:17 +0100

    change tree models

--- src/components/TreesMap/mapTreeModelUtil.ts
+++ src/components/TreesMap/mapTreeModelUtil.ts
@@ -4,9 +4,9 @@ export const getTreeModel = (): any => {
     ['<', ['number', ['get', 'age'], 320], 100],
     'tree-0-model',
     ['<', ['number', ['get', 'age'], 320], 200],
-    'tree-1-model',
+    'tree-2-model',
     ['<', ['number', ['get', 'age'], 320], 320],
-    'tree-2-model',
-    'tree-2-model',
+    'tree-1-model',
+    'tree-1-model',
   ];
 };
