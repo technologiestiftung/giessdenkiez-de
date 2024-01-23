9b9b3e8d6b0038cba2284621ad17b51ddcb4747b
Raphael Arce <raphael.arce@ts.berlin>
2024-01-23 12:27:07 +0100

    chore: fix tests after migration to vitest
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- src/utils/requestUtil.test.ts
+++ src/utils/requestUtil.test.ts
@@ -1,9 +1,28 @@
 import { requests } from './requestUtil';
+import { MockInstance } from 'vitest';
+
 describe('utility function requests', () => {
+  let fetchSpy: MockInstance<
+    [input: string | URL | Request, init?: RequestInit | undefined],
+    Promise<Response>
+  >;
+
+  beforeEach(() => {
+    fetchSpy = vi.spyOn(global, 'fetch');
+  });
+
   test('should call fetch using the provided url and json map header', () => {
-    vi.spyOn(global, 'fetch');
-    requests('http://some-url.com/');
-    expect(global.fetch).toHaveBeenLastCalledWith('http://some-url.com/', {
+    fetchSpy.mockImplementationOnce(
+      () =>
+        (({
+          ok: true,
+          json: () => {},
+        } as unknown) as Promise<Response>)
+    );
+
+    requests('/some-path');
+
+    expect(global.fetch).toHaveBeenLastCalledWith('/some-path', {
       headers: new Headers({
         'content-type': 'application/json',
       }),
@@ -11,49 +30,53 @@ describe('utility function requests', () => {
   });
 
   test('should call fetch using the provided bearer token', () => {
-    vi.spyOn(global, 'fetch');
-    requests('http://some-url.com/', { token: 'gepetto' });
+    fetchSpy.mockImplementationOnce(
+      () =>
+        (({
+          ok: true,
+          json: () => {},
+        } as unknown) as Promise<Response>)
+    );
 
-    expect(global.fetch).toHaveBeenLastCalledWith('http://some-url.com/', {
+    requests('/some-path', { token: 'gepetto' });
+
+    expect(global.fetch).toHaveBeenLastCalledWith('/some-path', {
       headers: new Headers({
         'content-type': 'application/json',
         authorization: 'Bearer gepetto',
       }),
     });
   });
+
   test('should return the response as json', async () => {
-    // @ts-ignore
-    vi.spyOn(global, 'fetch').mockImplementation(() =>
-      Promise.resolve({
-        ok: true,
-        json: () => Promise.resolve({ that: { is: 'myresponse' } }),
-      })
+    fetchSpy.mockImplementationOnce(
+      () =>
+        Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve({ that: { is: 'myresponse' } }),
+        }) as Promise<Response>
     );
-    const res = await requests<{ that: { is: string } }>(
-      'http://some-url.com/'
-    );
+
+    const res = await requests<{ that: { is: string } }>('/some-path');
+
     expect(res?.that?.is).toBe('myresponse');
   });
+
   test('should throw if the response is not ok', async () => {
-    // @ts-ignore
-    vi.spyOn(global, 'fetch').mockImplementation(() =>
-      Promise.resolve({
-        ok: false,
-        text: () => Promise.resolve('Pinocchio! Che ho dico! Non e buono!'),
-      })
+    fetchSpy.mockImplementationOnce(
+      () =>
+        Promise.resolve({
+          ok: false,
+          text: () => Promise.resolve('some error'),
+        }) as Promise<Response>
     );
-    await expect(requests('http://some-url.com/')).rejects.toThrow(
-      'Pinocchio! Che ho dico! Non e buono!'
-    );
+
+    await expect(requests('/some-path')).rejects.toThrow('some error');
   });
+
   test('should throw if the parameters are invalid', async () => {
+    fetchSpy.mockImplementationOnce(() => Promise.reject('missing params'));
     // @ts-ignore
-    vi.spyOn(global, 'fetch').mockImplementation(() =>
-      Promise.reject('Gepetto has no wood left to build pinocchio')
-    );
-    // @ts-ignore
-    await expect(requests()).rejects.toThrow(
-      'Gepetto has no wood left to build pinocchio'
-    );
+    await expect(requests()).rejects.toThrow('missing params');
   });
 });
