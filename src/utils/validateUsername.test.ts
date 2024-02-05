d081bf0eb2747af4f762779daedd9f7579406dca
ff6347 <hash@ff6347.email>
2024-02-05 14:02:25 +0100

    test: Fix tests for username validation util

--- src/utils/validateUsername.test.ts
+++ src/utils/validateUsername.test.ts
@@ -1,38 +1,46 @@
 import { validateUsername } from './validateUsername';
 
 describe('Username validation', () => {
+  // since this is only a unit we don't look up used names in the database
+  // and taken will always be false
+  const validExpectTaken = {
+    allowedLength: true,
+    minLength: true,
+    maxLength: true,
+    allowedCharacters: true,
+    taken: false,
+  };
   test('Validates username with minimum criteria', () => {
     const username = 'abc';
-    const { usernameIsValid } = validateUsername(username);
-    expect(usernameIsValid).toBe(true);
+    const { patterns } = validateUsername(username);
+    expect(patterns.minLength).toBe(true);
+    expect(patterns).toMatchObject(validExpectTaken);
   });
   test('Validates username with maximim criteria', () => {
     const username = 'abcdefghijklmnopqrstabcdefghijklmnopqrst1234567890';
-    const { usernameIsValid } = validateUsername(username);
-    expect(usernameIsValid).toBe(true);
+    const { patterns } = validateUsername(username);
+    expect(patterns.maxLength).toBe(true);
+    expect(patterns).toMatchObject(validExpectTaken);
   });
-
+  test('Allow username with spaces', () => {
+    const username = 'abc def';
+    const { patterns } = validateUsername(username);
+    expect(patterns.allowedCharacters).toBe(true);
+    expect(patterns).toMatchObject(validExpectTaken);
+  });
   test('Rejects username with fewer than 3 characters', () => {
     const username = 'ab';
     const { usernameIsValid } = validateUsername(username);
     expect(usernameIsValid).toBe(false);
   });
-
   test('Rejects username with more than 50 characters', () => {
     const username = 'abcdefghijklmnopqrstabcdefghijklmnopqrst12345678901';
     const { usernameIsValid } = validateUsername(username);
     expect(usernameIsValid).toBe(false);
   });
-
   test('Rejects username with special characters', () => {
     const username = 'abc@';
     const { usernameIsValid } = validateUsername(username);
     expect(usernameIsValid).toBe(false);
-  });
-
-  test('Allow username with spaces', () => {
-    const username = 'abc def';
-    const { usernameIsValid } = validateUsername(username);
-    expect(usernameIsValid).toBe(true);
   });
 });
