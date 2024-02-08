fc55140329c3122f50a6d6d262c95c756db0ebec
Raphael.A <8709861+raphael-arce@users.noreply.github.com>
2024-02-08 16:57:55 +0100

    feat: add styling to error page (#686)
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { useState } from 'react';
 import type { AppProps } from 'next/app';
 import { Page } from '../src/nextPage';
 import { Providers } from '../src/Providers';
-import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 import { useMatomo } from '../src/utils/hooks/useMatomo';
+import '../src/assets/styles.css';
+
 type PagePropsType = {
   treeId?: string | null;
 };
@@ -18,9 +18,8 @@ export default function MyApp({
   Component,
   pageProps,
 }: AppPropsType): JSX.Element {
-  const [supabaseClient] = useState(() => createPagesBrowserClient());
   useMatomo();
-  const getLayout = Component.getLayout ?? ((page) => page);
+  const getLayout = Component.getLayout ?? (page => page);
   const Layout = Component.layout ?? (({ children }) => <>{children}</>);
   return (
     <Providers>
