45fcceca30fd6ffffe70ea3ad5fa1ac211b77832
Jan Werner <jan.werner@guetersloh.de>
2024-02-19 16:21:41 +0100

    fix cors

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: '/api/:path*',
+        source: '*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
