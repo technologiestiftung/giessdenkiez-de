1c31dd88a375bb65c971563e0c4a3e608197b551
Raphael.A <8709861+raphael-arce@users.noreply.github.com>
2024-03-01 10:07:00 +0100

    feat: add sales pages link to home splash screen (#704)
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- src/components/Overlay/OverlayDescription/index.tsx
+++ src/components/Overlay/OverlayDescription/index.tsx
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 import OverlayParagraph from '../OverlayParagraph';
 
 const Wrapper = styled.div`
-  margin: -10px 40px 24px;
-  max-width: 64ch;
+  margin: -10px 40px 10px;
+  max-width: 100%;
 `;
 
 const OverlayDescription: FC<{ content: string[] }> = ({ content }) => (
