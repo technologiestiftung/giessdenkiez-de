dfc5ae150d91673d556d777eee20d001f4bdc644
Raphael Arce <raphael.arce@ts.berlin>
2024-03-04 13:57:17 +0100

    feat: initial setup for UI rework
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyComponent: React.FC = () => {
+  return (
+    <>
+      <h1 className="text-4xl">Hello, Giess den Kiez</h1>
+    </>
+  );
+};
+
+export default MyComponent;
--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: ['test/vitest.setup.js', 'dotenv/config'],
-  },
-});
