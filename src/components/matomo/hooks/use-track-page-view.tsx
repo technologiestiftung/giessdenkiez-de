1b88f47153461d01933e7da1d72dbab2980354c9
Raphael Arce <raphael.arce@ts.berlin>
2024-03-04 19:06:05 +0100

    feat: first draft of routing, state management + examples
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- src/components/matomo/hooks/use-track-page-view.tsx
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-import { trackPageView } from "../utils/matomo.ts";
-
-export function useTrackPageView() {
-  useEffect(() => {
-    trackPageView();
-  }, []);
-}
