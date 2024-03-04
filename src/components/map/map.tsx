084af54c40041d0d7bcf92a21ed80ac426d19597
Raphael Arce <raphael.arce@ts.berlin>
2024-03-04 15:03:40 +0100

    feat: add mapbox, minimal map setup and updated all dependencies to latest version
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

---
+++ src/components/map/map.tsx
@@ -0,0 +1,20 @@
+import React, { useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import { useMapSetup } from "./hooks/use-map-setup";
+
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_KEY;
+
+const Map: React.FC = () => {
+  const mapContainer = useRef<HTMLDivElement | null>(null);
+  const map = useRef<mapboxgl.Map | null>(null);
+
+  useMapSetup(mapContainer, map);
+
+  return (
+    <>
+      <div ref={mapContainer} className="h-screen" />
+    </>
+  );
+};
+
+export default Map;
