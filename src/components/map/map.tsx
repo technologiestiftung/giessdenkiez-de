c0309b47fee599e3c26ab0932a17aca3dffb2b31
Raphael Arce <raphael.arce@ts.berlin>
2024-03-06 19:25:45 +0100

    feat: add mapbox controls
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,6 +1,7 @@
 import mapboxgl from "mapbox-gl";
 import React, { useRef } from "react";
 import { useMapSetup } from "./hooks/use-map-setup";
+import MapAttribution from "./map-attribution";
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_KEY;
 
@@ -9,7 +10,14 @@ const Map: React.FC = () => {
 
   useMapSetup(mapContainer);
 
-  return <div ref={mapContainer} className="h-screen" />;
+  return (
+    <div className="grid grid-cols-1 grid-rows-1">
+      <div className="pointer-events-none z-[1000] col-start-1 row-start-1 flex items-end justify-end">
+        <MapAttribution />
+      </div>
+      <div ref={mapContainer} className="col-start-1 row-start-1 h-screen" />
+    </div>
+  );
 };
 
 export default Map;
