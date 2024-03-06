4767b377b527a0ef10b0b1c59ee90ea301757f35
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-03-06 15:03:10 +0100

    chore: fix imports

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,7 +1,6 @@
 import mapboxgl from "mapbox-gl";
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { useMapSetup } from "./hooks/use-map-setup";
-import { useMapStore } from "./map-store";
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_KEY;
 
