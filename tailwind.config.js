6af0732fe50ccbbe1712173218181294c64ac255
aeschi <anna.eschenbacher@icloud.com>
2024-03-07 11:13:31 +0100

    feat: add gdk colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gdk-gray": "#2C303B",
+        "gdk-white": "#FEFEFE",
+        "gdk-blue": "#1169EE",
+        "gdk-light-blue": "#96BCF4",
+        "gdk-lighter-blue": "#E7F0FD",
+        "gdk-red": "#BD0909",
+        "gdk-neon-green": "#3DF99A",
+        "gdk-tree-green": "#CAE11F",
+        "gdk-dark-blue": "#0948A7",
+        "gdk-dark-green": "#07964E",
+        "gdk-purple": "#660A9C",
+      },
+    },
   },
   plugins: [],
 };
