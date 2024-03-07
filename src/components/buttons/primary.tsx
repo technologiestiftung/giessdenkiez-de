c586b2bfdd1a87052b81ecdec48455876e4ff688
aeschi <56318362+aeschi@users.noreply.github.com>
2024-03-07 16:19:39 +0100

    Feat/define buttons and colors (#718)
    * feat: add gdk colors
    
    * feat: add button components
    
    * feat: add option for icon within button components
    
    * feat: added necessary font weights and default text color
    
    * feat: remove margin from tertiary button
    
    * feat: remove woff files remove pointer-event and refactor destructive buttons
    
    * feat: cleanup profile tsx

---
+++ src/components/buttons/primary.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export interface PrimaryButtonProps {
+  label: string | React.ReactNode;
+  onClick: () => void;
+  disabled?: boolean;
+}
+
+const PrimaryButton: React.FC<PrimaryButtonProps> = ({
+  label,
+  onClick,
+  disabled,
+}) => {
+  return (
+    <button
+      className={`text-gdk-white bg-gdk-blue hover:bg-gdk-light-blue disabled:bg-gdk-light-gray 
+      my-4 flex h-[51px] w-fit items-center justify-center rounded-[10px] px-8 font-semibold`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      <span className="flex flex-row  items-center gap-3">{label}</span>
+    </button>
+  );
+};
+
+export default PrimaryButton;
