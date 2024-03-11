e9a918e4c483ee6be0671d13a65f61898a5d297a
aeschi <anna.eschenbacher@icloud.com>
2024-03-11 10:50:35 +0100

    feat: add edit icons and buttons

--- src/components/profile/profile-details.tsx
+++ src/components/profile/profile-details.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useI18nStore } from "../../i18n/i18n-store.ts";
+import EditIcon from "../icons/edit-icon.tsx";
+import TertiaryButton from "../buttons/tertiary.tsx";
 
 const ProfileDetails: React.FC = () => {
   const i18n = useI18nStore().i18n();
@@ -11,29 +13,51 @@ const ProfileDetails: React.FC = () => {
       </h2>
 
       <div className="mt-7 flex flex-col">
-        <label className="font-semibold" htmlFor="username">
+        <label className="font-semibold " htmlFor="username">
           {i18n.navbar.profile.settings.username}
         </label>
-        <input className="italic" id="username" type="text" value="Username" />
+        <div className="flex flex-row justify-between gap-x-4 ">
+          <input
+            className="flex-auto italic disabled:bg-gdk-white"
+            id="username"
+            type="text"
+            value="Username"
+          />
+          <button className=" self-end text-gdk-blue">
+            <EditIcon />
+          </button>
+        </div>
       </div>
 
       <div className="mt-4 flex flex-col">
         <label className="font-semibold" htmlFor="email">
           {i18n.navbar.profile.settings.email}
         </label>
-        <input
-          className="italic"
-          id="email"
-          type="email"
-          value="xyz@ts.berlin"
-        />
+        <div className="flex flex-row justify-between gap-x-4 ">
+          <input
+            className="flex-auto italic disabled:bg-gdk-white"
+            id="email"
+            type="email"
+            value="xyz@ts.berlin"
+            disabled={true}
+          />
+          <button className=" self-end text-gdk-blue">
+            <EditIcon />
+          </button>
+        </div>
       </div>
 
       <div className="mt-1 flex flex-col">
         <label className="font-semibold" htmlFor="email">
           {i18n.navbar.profile.settings.password}
         </label>
-        <input id="password" type="password" value="1234567890" />
+        <div className="flex flex-row justify-between gap-x-4 ">
+          <input id="password" type="password" value="1234567890" />
+          <TertiaryButton
+            onClick={() => {}}
+            label="Passwort ändern"
+          ></TertiaryButton>
+        </div>
       </div>
     </>
   );
