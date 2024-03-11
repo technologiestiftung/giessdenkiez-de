9a5261799b720aeadae8bd44c3b2127b41090cac
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-03-11 10:35:29 +0100

    feat: info page (#715)
    * feat: initial setup for i18n
    
    * feat: first draft info page
    
    * feat: faq section
    
    * feat: close button
    
    * feat: social share
    
    * chore: refactoring
    
    * chore: remove console log
    
    * fix: colors, font weights, centering
    
    * fix: border
    
    * fix: big slack button
    
    * feat: add children props to qa entry, increase stroke width for chevrons

---
+++ src/components/info/social-share.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  FacebookIcon,
+  FacebookShareButton,
+  TwitterShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+  XIcon,
+  EmailShareButton,
+} from "react-share";
+import { useI18nStore } from "../../i18n/i18n-store";
+import Markdown from "react-markdown";
+
+const SocialShare: React.FC = () => {
+  const i18n = useI18nStore().i18n();
+  return (
+    <div className="flex w-full flex-col items-center justify-center text-center">
+      <div className="text-gdk-gray flex w-[60%] flex-col gap-4">
+        <div>{i18n.info.share.title}</div>
+        <div className="flex flex-row justify-center gap-2">
+          <FacebookShareButton
+            aria-label="facebook-sharing-button"
+            url="https://www.giessdenkiez.de/"
+          >
+            <FacebookIcon size={36} round />
+          </FacebookShareButton>
+          <TwitterShareButton
+            aria-label="x-sharing-button"
+            title={i18n.info.share.content}
+            url="https://www.giessdenkiez.de/"
+          >
+            <XIcon size={36} round />
+          </TwitterShareButton>
+          <WhatsappShareButton
+            aria-label="whatsapp-sharing-button"
+            title={i18n.info.share.content}
+            url="https://www.giessdenkiez.de/"
+          >
+            <WhatsappIcon size={36} round />
+          </WhatsappShareButton>
+          <EmailShareButton
+            aria-label="mail-sharing-button"
+            url="https://www.giessdenkiez.de/"
+            body={i18n.info.share.content}
+          >
+            <img
+              src={"images/mail_icon.svg"}
+              width={36}
+              height={36}
+              alt="Mail Icon"
+            />
+          </EmailShareButton>
+        </div>
+        <Markdown className={"[&>p>a]:underline"}>
+          {i18n.info.share.openSource}
+        </Markdown>
+      </div>
+    </div>
+  );
+};
+
+export default SocialShare;
--- src/i18n/content-types.ts
+++ src/i18n/content-types.ts
@@ -12,8 +12,31 @@ interface NotFound {
   title: string;
 }
 
+interface QuestionAnswer {
+  question: string;
+  answer: string;
+}
+
+interface Info {
+  about: {
+    head: QuestionAnswer;
+    qa: QuestionAnswer[];
+  };
+  faq: {
+    title: string;
+    description: string;
+    qa: QuestionAnswer[];
+  };
+  share: {
+    title: string;
+    content: string;
+    openSource: string;
+  };
+}
+
 export interface Content {
   locationSearch: LocationSearch;
   navbar: Navbar;
   notFound: NotFound;
+  info: Info;
 }
