04f4d69fa18eb9b7bea4135d2a52fe9a94123325
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-03-11 10:44:59 +0100

    feat: move map constants to env

--- src/components/map/hooks/use-map-constants.tsx
+++ src/components/map/hooks/use-map-constants.tsx
@@ -4,8 +4,8 @@ import tailwindConfig from "../../../../tailwind.config.js";
 const fullConfig = resolveConfig(tailwindConfig);
 
 export function useMapConstants() {
-  const MAP_PITCH_DEGREES = 45;
-  const ZOOMED_IN_ZOOM_LEVEL = 20;
+  const MAP_PITCH_DEGREES = import.meta.env.VITE_MAP_PITCH_DEGREES;
+  const ZOOMED_IN_ZOOM_LEVEL = import.meta.env.VITE_MAP_MAX_ZOOM_LEVEL;
   const TREE_DEFAULT_COLOR = fullConfig.theme.colors["gdk-tree-green"];
   return { MAP_PITCH_DEGREES, ZOOMED_IN_ZOOM_LEVEL, TREE_DEFAULT_COLOR };
 }
