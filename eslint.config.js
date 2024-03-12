bd478772a666b004833ad6ef305fe74e296834ba
ff6347 <hash@ff6347.email>
2024-03-12 10:29:26 +0100

    chore: Setup eslint further
    - remove prettier config
    - use react recommended
    - use own plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import globals from "globals";
-import technologiestiftung from "@technologiestiftung/eslint-config";
-import react from "eslint-plugin-react";
+import tsbConfig from "@technologiestiftung/eslint-config";
+import { plugin as tsbPlugin } from "@technologiestiftung/eslint-plugin";
+import reactRecommended from "eslint-plugin-react/configs/recommended.js";
 
 export default [
-  ...technologiestiftung,
+  ...tsbConfig,
+  reactRecommended,
   {
     files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
     languageOptions: {
@@ -16,7 +18,9 @@ export default [
         ...globals.browser,
       },
     },
-    rules: {},
-    plugins: { react },
+    rules: {
+      "@technologiestiftung/no-default-export": "error",
+    },
+    plugins: { "@technologiestiftung": tsbPlugin },
   },
 ];
