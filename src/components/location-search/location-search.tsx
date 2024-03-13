a51113ba77175b4021b15377e14ebe68316e2d2c
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-03-13 14:38:31 +0100

    feat: use arrow down / up keys to navigate search results, use enter to select

--- src/components/location-search/location-search.tsx
+++ src/components/location-search/location-search.tsx
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useI18nStore } from "../../i18n/i18n-store";
 import ClearIcon from "../icons/clear-icon";
 import SearchIcon from "../icons/search-icon";
+import { useMapConstants } from "../map/hooks/use-map-constants";
 import { useMapStore } from "../map/map-store";
 import { GeocodingResult, useGeocoding } from "./hooks/use-geocoding";
-import { useMapConstants } from "../map/hooks/use-map-constants";
 
 const LocationSearch: React.FC = () => {
   const i18n = useI18nStore().i18n();
 
   const [search, setSearch] = useState("");
+  const [selectedGeocodingResultIndex, setSelectedGeocodingResultIndex] =
+    useState(0);
+
   const [selectedGeocodingResult, setSelectedGeocodingResult] =
     useState<GeocodingResult>();
 
@@ -48,6 +51,31 @@ const LocationSearch: React.FC = () => {
     clearGeocodingResults();
   };
 
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      if (event.key === "ArrowDown") {
+        setSelectedGeocodingResultIndex(
+          Math.min(
+            Math.max(0, geocodingResults.length - 1),
+            selectedGeocodingResultIndex + 1,
+          ),
+        );
+      } else if (event.key === "ArrowUp") {
+        setSelectedGeocodingResultIndex(
+          Math.max(0, selectedGeocodingResultIndex - 1),
+        );
+      } else if (event.key === "Enter") {
+        onGeocodingResultClick(geocodingResults[selectedGeocodingResultIndex]);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [geocodingResults, selectedGeocodingResultIndex]);
+
   return (
     <div className="mt-2 flex w-full justify-center">
       <div
@@ -60,12 +88,17 @@ const LocationSearch: React.FC = () => {
           className={` z-[2] flex flex-row items-center justify-center rounded-full bg-white`}
         >
           <button className="pl-4">
-            <SearchIcon></SearchIcon>
+            <SearchIcon />
           </button>
           <input
             className={`w-full py-4 pl-2 focus:outline-none`}
             type="text"
             value={selectedGeocodingResult?.place_name_de || search}
+            onKeyDown={(e) => {
+              if (e.key === "ArrowDown" || e.key === "ArrowUp") {
+                e.preventDefault();
+              }
+            }}
             onChange={(e) => {
               setSelectedGeocodingResult(undefined);
               setSearch(e.target.value);
@@ -73,7 +106,7 @@ const LocationSearch: React.FC = () => {
             placeholder={i18n.locationSearch.placeholder}
           />
           <button className="px-4" onClick={clearSearch}>
-            <ClearIcon></ClearIcon>
+            <ClearIcon />
           </button>
         </form>
 
@@ -82,7 +115,7 @@ const LocationSearch: React.FC = () => {
             {geocodingResults.map((geocodingResult, idx) => (
               <button
                 key={`geocoding-result-${idx}`}
-                className=" truncate px-4 py-4 text-left hover:cursor-pointer hover:bg-sky-100"
+                className={`truncate px-4 py-4 text-left hover:cursor-pointer hover:bg-sky-100 ${selectedGeocodingResultIndex === idx && "bg-sky-100"}`}
                 onClick={() => onGeocodingResultClick(geocodingResult)}
               >
                 {geocodingResult.place_name_de}
