bd1934a7af0e1918b8f35eb2bb31a620560464ae
aeschi <anna.eschenbacher@icloud.com>
2024-03-21 13:49:02 +0100

    feat: cleanup

--- src/components/buttons/adoptButton.tsx
+++ src/components/buttons/adoptButton.tsx
@@ -36,16 +36,3 @@ export const AdoptButton: React.FC<AdoptButtonProps> = ({ treeId }) => {
 		</button>
 	);
 };
-
-{
-	/* <button
-className={`
-my-4 flex h-[51px] w-full items-center justify-center rounded-[10px] bg-gdk-blue px-8 font-semibold 
-text-gdk-white hover:bg-gdk-light-blue disabled:bg-gdk-light-gray sm:w-fit`}
-disabled={disabled}
-onClick={onClick}
-type={type}
->
-<span className="flex flex-row items-center gap-3">{label}</span>
-</button> */
-}
