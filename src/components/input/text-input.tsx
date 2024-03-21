76e3689ee5f074120481ffa9ad027c0b35501654
Raphael Arce <raphael.arce@ts.berlin>
2024-03-21 18:19:19 +0100

    feat: make text of inputs of size >16px to prevent zoom in on mobile
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- src/components/input/text-input.tsx
+++ src/components/input/text-input.tsx
@@ -7,7 +7,7 @@ export const TextInput: React.FC<
 		<input
 			className={`
         border-input bg-background placeholder:text-muted-foreground my-2 flex h-[51px] w-full flex-auto 
-        rounded-2xl  border px-3 py-2 text-sm outline-1 focus:outline-2
+        rounded-2xl border px-3 py-2 outline-1 focus:outline-2
         focus:outline-gdk-blue disabled:cursor-not-allowed disabled:opacity-50
       `}
 			{...props}
