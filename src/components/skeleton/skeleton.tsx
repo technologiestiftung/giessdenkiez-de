60de30cf5d54de1b1fb48371efaf7d5d3afee1fd
aeschi <anna.eschenbacher@icloud.com>
2024-03-25 17:11:18 +0100

    feat: add skeletopn component

---
+++ src/components/skeleton/skeleton.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export interface SkeletonProps {
+	className?: string;
+}
+
+export const Skeleton: React.FC<
+	SkeletonProps & React.HTMLAttributes<HTMLDivElement>
+> = ({ className, ...props }) => {
+	return (
+		<div
+			className={"animate-pulse rounded-md bg-muted" + className}
+			{...props}
+		/>
+	);
+};
