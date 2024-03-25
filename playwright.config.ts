a56cb2b5712fe8a777be4e1e34f907cbbdb02d81
Raphael Arce <raphael.arce@ts.berlin>
2024-03-25 16:30:07 +0100

    ci: raised timeout
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on",
 	},
-	timeout: 30000,
+	timeout: 60000,
 
 	/* Configure projects for major browsers */
 	projects: [
