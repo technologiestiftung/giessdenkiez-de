65bd950c90b8397a8669f46f224a7fc952533697
aeschi <anna.eschenbacher@icloud.com>
2024-03-26 14:36:44 +0100

    feat: add Skeleton for Overview in profile

--- src/components/profile/profile-logged-in/overview.tsx
+++ src/components/profile/profile-logged-in/overview.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useI18nStore } from "../../../i18n/i18n-store";
 import { useAuthStore } from "../../../auth/auth-store";
 import { AdoptedTreeIcon } from "../../icons/adopted-tree-icon";
+import { Skeleton } from "../../skeleton/skeleton";
 
 export const Overview: React.FC = () => {
 	const i18n = useI18nStore().i18n();
@@ -26,32 +27,44 @@ export const Overview: React.FC = () => {
 			</h2>
 
 			<div className="mt-7 flex flex-col gap-3 lg:flex-row ">
-				<div className="shadow-gdk-soft  flex flex-col justify-between gap-3 lg:min-w-36 rounded-2xl border-2 p-4 font-semibold">
-					{i18n.navbar.profile.overview.liter}
-					<span className="flex items-baseline gap-x-5 text-5xl font-medium">
-						<img src="images/icon-drop.svg" alt="" className="w-5" />
-						{formatNumber(wateringAmountTotal ?? 0)}
-					</span>
-				</div>
+				{adoptedTreesInfo === null ? (
+					<>
+						<Skeleton className="h-[120px] lg:h-[144px] shadow-gdk-soft lg:min-w-36 rounded-2xl border-2 p-4" />
+						<div className="flex gap-3">
+							<Skeleton className="h-[120px] lg:h-[144px] shadow-gdk-soft w-full lg:min-w-36 rounded-2xl border-2 p-4" />
+							<Skeleton className="h-[120px] lg:h-[144px] shadow-gdk-soft w-full lg:min-w-36 rounded-2xl border-2 p-4" />
+						</div>
+					</>
+				) : (
+					<>
+						<div className="shadow-gdk-soft  flex flex-col justify-between gap-3 lg:min-w-36 rounded-2xl border-2 p-4 font-semibold">
+							{i18n.navbar.profile.overview.liter}
+							<span className="flex items-baseline gap-x-5 text-5xl font-medium">
+								<img src="images/icon-drop.svg" alt="" className="w-5" />
+								{formatNumber(wateringAmountTotal ?? 0)}
+							</span>
+						</div>
 
-				<div className="flex gap-3">
-					<div className="shadow-gdk-soft flex w-full flex-col justify-between gap-3 lg:min-w-36 rounded-2xl border-2 p-4 font-semibold">
-						{i18n.navbar.profile.overview.irrigations}
-						<span className="flex items-baseline gap-x-5 text-5xl font-medium">
-							<img src="images/icon-watering-can.svg" alt="" className="" />
-							{formatNumber(wateringCountTotal ?? 0)}
-						</span>
-					</div>
-					<div className="shadow-gdk-soft flex w-full flex-col justify-between gap-3 lg:min-w-36 rounded-2xl border-2 p-4 font-semibold">
-						{i18n.navbar.profile.overview.adoptedTrees}
-						<span className="flex items-baseline gap-x-5 text-5xl font-medium">
-							<div className="text-gdk-purple stroke-none">
-								<AdoptedTreeIcon />
+						<div className="flex gap-3">
+							<div className="shadow-gdk-soft flex w-full flex-col justify-between gap-3 lg:min-w-36 rounded-2xl border-2 p-4 font-semibold">
+								{i18n.navbar.profile.overview.irrigations}
+								<span className="flex items-baseline gap-x-5 text-5xl font-medium">
+									<img src="images/icon-watering-can.svg" alt="" className="" />
+									{formatNumber(wateringCountTotal ?? 0)}
+								</span>
 							</div>
-							{formatNumber(adoptedTrees?.length)}
-						</span>
-					</div>
-				</div>
+							<div className="shadow-gdk-soft flex w-full flex-col justify-between gap-3 lg:min-w-36 rounded-2xl border-2 p-4 font-semibold">
+								{i18n.navbar.profile.overview.adoptedTrees}
+								<span className="flex items-baseline gap-x-5 text-5xl font-medium">
+									<div className="text-gdk-purple stroke-none">
+										<AdoptedTreeIcon />
+									</div>
+									{formatNumber(adoptedTrees?.length)}
+								</span>
+							</div>
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	);
