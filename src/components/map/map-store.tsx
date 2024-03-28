78511d7df7695dd8c28babc21f4099a261d44dfd
aeschi <56318362+aeschi@users.noreply.github.com>
2024-03-28 09:05:36 +0100

    Feat/loading screens for map and tree info (#803)
    * feat: add loading screen and map loading state
    
    * feat: add loading screen to app.tsx
    
    * feat: add internationalization
    
    * feat: add loading tree information
    
    * feat: add privacy and imprint
    
    * fix: pointer events
    
    * fix: line break for english version
    
    * feat: cleanup comments

--- src/components/map/map-store.tsx
+++ src/components/map/map-store.tsx
@@ -4,11 +4,17 @@ import { create } from "zustand";
 interface MapState {
 	map: mapboxgl.Map | undefined;
 	setMap: (map: mapboxgl.Map | undefined) => void;
+	isMapLoaded: boolean;
+	setIsMapLoaded: (isMapLoaded: boolean) => void;
 }
 
 export const useMapStore = create<MapState>()((set) => ({
 	map: undefined,
 	setMap: (map: mapboxgl.Map | undefined) => {
 		set({ map });
+	},
+	isMapLoaded: false,
+	setIsMapLoaded: (isMapLoaded: boolean) => {
+		set({ isMapLoaded });
 	},
 }));
