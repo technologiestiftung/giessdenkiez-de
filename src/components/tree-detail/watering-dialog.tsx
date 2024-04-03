10e8159e0f67aed1148df9f50b5364a8b467bdf6
Raphael Arce <raphael.arce@ts.berlin>
2024-04-03 12:04:41 +0200

    feat: add x button to close watering dialog
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- src/components/tree-detail/watering-dialog.tsx
+++ src/components/tree-detail/watering-dialog.tsx
@@ -1,12 +1,12 @@
 import React, { useCallback } from "react";
 import { useI18nStore } from "../../i18n/i18n-store";
-import "../../index.css";
 import { PrimaryButton } from "../buttons/primary";
 import { useWaterTree } from "./hooks/use-water-tree";
 import { TreeData } from "./tree-types";
 import { useErrorStore } from "../../error/error-store";
 import { format } from "date-fns";
 import { TertiaryButton } from "../buttons/tertiary";
+import { CloseIcon } from "../icons/close-icon";
 interface WateringDialogProps {
 	treeData: TreeData;
 	close: () => void;
@@ -57,55 +57,69 @@ export const WateringDialog: React.FC<WateringDialogProps> = ({
 	return (
 		<dialog
 			id="water-dialog"
-			className="flex-col rounded-lg w-11/12 sm:w-[400px]"
+			className="flex-col rounded-lg w-11/12 sm:w-[400px] backdrop:backdrop-brightness-90"
 			onClick={onDialogClick}
 		>
-			<form onSubmit={onSubmit}>
-				<div className="flex flex-col gap-6 p-8">
-					<div className="text-xl font-bold">
-						{i18n.treeDetail.waterNeed.submitWatering}
-					</div>
-					<div className="flex flex-col gap-2">
-						<label className="text-lg font-semibold" htmlFor="amount">
-							{i18n.treeDetail.waterNeed.wateredHowMuch}
-						</label>
-						<input
-							className="rounded-lg border-2 p-4"
-							type="number"
-							inputMode="numeric"
-							placeholder={i18n.treeDetail.waterNeed.wateredHowMuchPlaceholder}
-							required
-							name="amount"
-							min="1"
-						/>
-					</div>
-					<div className="flex flex-col gap-2">
-						<label className="text-lg font-semibold" htmlFor="date">
-							{i18n.treeDetail.waterNeed.wateredWhen}
-						</label>
-						<input
-							className="rounded-lg border-2 p-4"
-							type="date"
-							name="date"
-							required
-							defaultValue={formattedToday}
-							max={formattedToday}
-						/>
-					</div>
-					<div className="flex flex-col-reverse sm:flex-row justify-between gap-x-4">
-						<div className="p-y-3.5 flex self-center">
-							<TertiaryButton
-								label={i18n.treeDetail.waterNeed.waterCancel}
-								onClick={close}
+			<div className="grid grid-cols-1">
+				<form
+					onSubmit={onSubmit}
+					className="col-start-1 row-start-1 h-full w-full"
+				>
+					<div className="flex flex-col gap-6 p-8">
+						<div className="text-xl font-bold">
+							{i18n.treeDetail.waterNeed.submitWatering}
+						</div>
+						<div className="flex flex-col gap-2">
+							<label className="text-lg font-semibold" htmlFor="amount">
+								{i18n.treeDetail.waterNeed.wateredHowMuch}
+							</label>
+							<input
+								className="rounded-lg border-2 p-4"
+								type="number"
+								inputMode="numeric"
+								placeholder={
+									i18n.treeDetail.waterNeed.wateredHowMuchPlaceholder
+								}
+								required
+								name="amount"
+								min="1"
 							/>
 						</div>
-						<PrimaryButton
-							label={i18n.treeDetail.waterNeed.waterSave}
-							type="submit"
-						/>
+						<div className="flex flex-col gap-2">
+							<label className="text-lg font-semibold" htmlFor="date">
+								{i18n.treeDetail.waterNeed.wateredWhen}
+							</label>
+							<input
+								className="rounded-lg border-2 p-4"
+								type="date"
+								name="date"
+								required
+								defaultValue={formattedToday}
+								max={formattedToday}
+							/>
+						</div>
+						<div className="flex flex-col-reverse sm:flex-row justify-between gap-x-4">
+							<div className="p-y-3.5 flex self-center">
+								<TertiaryButton
+									label={i18n.treeDetail.waterNeed.waterCancel}
+									onClick={close}
+								/>
+							</div>
+							<PrimaryButton
+								label={i18n.treeDetail.waterNeed.waterSave}
+								type="submit"
+							/>
+						</div>
+					</div>
+				</form>
+				<div className="pointer-events-none col-start-1 row-start-1">
+					<div className="flex w-full justify-end">
+						<button className="pointer-events-auto p-2" onClick={close}>
+							<CloseIcon />
+						</button>
 					</div>
 				</div>
-			</form>
+			</div>
 		</dialog>
 	);
 };
