2543e734c3bffc11e8e0bcc2bf9a2a74fe9e9901
aeschi <anna.eschenbacher@icloud.com>
2024-04-04 14:25:30 +0200

    feat: change order of buttons on mobile and small css fix

--- src/components/buttons/secondary-destructive.tsx
+++ src/components/buttons/secondary-destructive.tsx
@@ -18,7 +18,7 @@ export const SecondaryDestructiveButton: React.FC<SecondaryButtonProps> = ({
 			className={`
 			outline-gdk-dark-red bg-gdk-white text-gdk-dark-red 
 			enabled:hover:bg-gdk-dark-red hover:text-gdk-white disabled:outline-gdk-light-gray disabled:text-gdk-light-gray 
-			my-4 flex h-[51px] w-full items-center justify-center rounded-[10px] px-8 py-3.5 font-semibold 
+			my-2 lg:my-4 flex h-[51px] w-full items-center justify-center rounded-[10px] px-8 py-3.5 font-semibold 
 			outline outline-2 sm:w-fit`}
 			disabled={disabled}
 			onClick={onClick}
