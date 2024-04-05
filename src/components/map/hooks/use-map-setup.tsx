3900afae1973c53b8696235ba79e14d076000739
Raphael Arce <raphael.arce@ts.berlin>
2024-04-05 15:42:19 +0200

    feat: remove live location updates when a user locates themself
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- src/components/map/hooks/use-map-setup.tsx
+++ src/components/map/hooks/use-map-setup.tsx
@@ -135,8 +135,6 @@ export function useMapSetup(
 			positionOptions: {
 				enableHighAccuracy: true,
 			},
-			// When active the map will receive updates to the device's location as it changes.
-			trackUserLocation: true,
 			// Draw an arrow next to the location dot to indicate which direction the device is heading.
 			showUserHeading: true,
 			showAccuracyCircle: false,
