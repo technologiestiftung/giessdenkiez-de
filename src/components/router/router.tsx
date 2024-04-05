fc5dfd871fe3cd41e36b4943b634a79c56836a07
aeschi <anna.eschenbacher@icloud.com>
2024-04-05 11:09:24 +0200

    feat: WIP changing splash to dialog

--- src/components/router/router.tsx
+++ src/components/router/router.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Filter } from "../filter/filter";
 import { useFilterStore } from "../filter/filter-store";
 import { Info } from "../info/info";
@@ -22,7 +22,6 @@ export const Router: React.FC = () => {
 	const { isMapLoaded } = useMapStore();
 
 	const { isFilterViewVisible, recoverUrlParams } = useFilterStore();
-	const [isSplashScreenVisible, setIsSplashScreenVisible] = useState(true);
 
 	useEffect(() => {
 		if (url.pathname === "/map") {
@@ -38,9 +37,8 @@ export const Router: React.FC = () => {
 		case "/map":
 			return (
 				<div
-					className={`flex h-svh w-screen flex-col-reverse justify-between lg:flex-row ${
-						treeId && "bg-white"
-					} lg:bg-transparent ${isSplashScreenVisible && "backdrop-brightness-90"}`}
+					className={`flex h-svh w-screen flex-col-reverse justify-between lg:bg-transparent lg:flex-row 
+					${treeId && "bg-white"} `}
 				>
 					<div
 						className={`${isFilterViewVisible && "bg-white rounded-t-lg sm:bg-transparent"}`}
@@ -51,7 +49,7 @@ export const Router: React.FC = () => {
 						<Navbar />
 					</div>
 
-					{!isSplashScreenVisible && isMapLoaded && (
+					{isMapLoaded && (
 						<div className="mt-2 flex w-full flex-row justify-center">
 							<div
 								className={`${
@@ -71,9 +69,14 @@ export const Router: React.FC = () => {
 						</div>
 					)}
 					{treeId && isMapLoaded && <TreeDetail />}
-					{isSplashScreenVisible && (
-						<Splash onClose={() => setIsSplashScreenVisible(false)} />
-					)}
+
+					<Splash
+						onClose={() =>
+							(
+								document.getElementById("splash-dialog") as HTMLDialogElement
+							).close()
+						}
+					/>
 				</div>
 			);
 		case "/profile/reset-password":
