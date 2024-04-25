068f4074ff47779ce090a600ebab7a9ca28a9102
aeschi <anna.eschenbacher@icloud.com>
2024-04-25 16:18:20 +0200

    feat: more cleanup

--- src/components/filter/age-range-slider/age-range-slider.tsx
+++ src/components/filter/age-range-slider/age-range-slider.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent } from "react";
 import { useFilterStore } from "../filter-store";
 import { useI18nStore } from "../../../i18n/i18n-store";
 
@@ -8,30 +8,15 @@ export const INITIAL_MAX = 200;
 
 export const AgeRangeSlider: React.FC = () => {
 	const { treeAgeRange, setTreeAgeRange } = useFilterStore();
-
-	const [min, setMin] = useState(treeAgeRange.min);
-	const [max, setMax] = useState(treeAgeRange.max);
-
 	const i18n = useI18nStore().i18n();
-
-	const resetAgeSlider = () => {
-		setMin(INITIAL_MIN);
-		setMax(INITIAL_MAX);
-	};
-
-	useEffect(() => {
-		if (treeAgeRange.min === INITIAL_MIN && treeAgeRange.max === INITIAL_MAX) {
-			resetAgeSlider();
-		}
-	}, [treeAgeRange]);
 
 	return (
 		<div className="flex flex-col w-full gap-6">
 			<div className="flex flex-row justify-between font-semibold text-lg">
 				<div>{i18n.filter.treeAgeTitle}</div>
 				<div>
-					{min}-{max}
-					{max === 200 ? "+" : ""} {i18n.filter.years}
+					{treeAgeRange.min}-{treeAgeRange.max}
+					{treeAgeRange.max === 200 ? "+" : ""} {i18n.filter.years}
 				</div>
 			</div>
 			<div className="relative">
@@ -39,31 +24,29 @@ export const AgeRangeSlider: React.FC = () => {
 					type="range"
 					min={INITIAL_MIN}
 					max={INITIAL_MAX}
-					value={min}
+					value={treeAgeRange.min}
 					step={10}
 					onChange={(event: ChangeEvent<HTMLInputElement>) => {
-						const value = Math.min(+event.target.value, max - 10);
-						setMin(value);
+						const value = Math.min(+event.target.value, treeAgeRange.max - 10);
 						event.target.value = value.toString();
-						setTreeAgeRange(value, max);
+						setTreeAgeRange(value, treeAgeRange.max);
 					}}
 					// slider-thumb styles are defined in index.css since stlyles are too long with thumb declerations
 					className={`w-full h-0 absolute border border-gdk-light-gray 
                      rounded-lg appearance-none pointer-events-none slider-thumb 
-                     ${min > INITIAL_MAX - 10 ? "thumb-50" : "thumb-30"}
+                     ${treeAgeRange.min > INITIAL_MAX - 10 ? "thumb-50" : "thumb-30"}
                    `}
 				/>
 				<input
 					type="range"
 					min={INITIAL_MIN}
 					max={INITIAL_MAX}
-					value={max}
+					value={treeAgeRange.max}
 					step={10}
 					onChange={(event: ChangeEvent<HTMLInputElement>) => {
-						const value = Math.max(+event.target.value, min + 10);
-						setMax(value);
+						const value = Math.max(+event.target.value, treeAgeRange.min + 10);
 						event.target.value = value.toString();
-						setTreeAgeRange(min, value);
+						setTreeAgeRange(treeAgeRange.min, value);
 					}}
 					className={`w-full h-0 absolute translate-y-[1px] rounded-lg 
 					appearance-none pointer-events-none slider-thumb thumb-40
