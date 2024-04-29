5dd5a0ea1161549777b013b8a308024faa5dd51a
Raphael Arce <raphael.arce@ts.berlin>
2024-04-29 20:24:59 +0200

    feat: align section title and link to adopted trees
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- src/components/profile/profile-logged-in/adopted-trees/adopted-trees-card.tsx
+++ src/components/profile/profile-logged-in/adopted-trees/adopted-trees-card.tsx
@@ -15,12 +15,13 @@ export const AdoptedTreesCard: React.FC<
 
 	return (
 		<div className="md:shadow-gdk-soft mb-3 md:rounded-2xl md:border-2 md:p-7">
-			<div className="flex justify-between">
+			<div className="flex items-center w-full justify-between">
 				<h2 className="text-2xl font-semibold">{sectionTitle}</h2>
 				<InternalAnchorLink
 					href="/map"
 					onClick={() => setAreOnlyMyAdoptedTreesVisible(true)}
 					label={i18n.navbar.profile.overview.showOnMap}
+					className="text-end"
 				/>
 			</div>
 			{props.children}
