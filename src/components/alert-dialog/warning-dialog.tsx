eae285e4081346e627df0b1fc26e7bf907014988
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-06-12 13:40:03 +0200

    fix: close on backdrop and x

--- src/components/alert-dialog/warning-dialog.tsx
+++ src/components/alert-dialog/warning-dialog.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Markdown from "react-markdown";
 import { PrimaryButton } from "../buttons/primary.tsx";
 import { InfoIcon } from "../icons/info-icon.tsx";
 import { useUrlState } from "../router/store.tsx";
+import { CloseIcon } from "../icons/close-icon";
 
 export interface WarningDialogProps {
 	title: string;
@@ -28,12 +29,24 @@ export const WarningDialog: React.FC<WarningDialogProps> = ({
 		}
 	};
 
+	const onDialogClick = useCallback(
+		(event: React.MouseEvent<HTMLDialogElement, MouseEvent>) => {
+			const isClickOnBackground = event.target !== document.getElementById(id);
+			if (isClickOnBackground) {
+				return;
+			}
+			(document.getElementById(id) as HTMLDialogElement).close();
+		},
+		[],
+	);
+
 	return (
 		<dialog
 			id={id}
-			className="py-6 px-10 bg-gdk-white shadow-gdk-hard rounded-2xl w-11/12 sm:w-[410px] backdrop:backdrop-blur-xs"
+			className=" bg-gdk-white shadow-gdk-hard rounded-2xl w-11/12 sm:w-[410px] backdrop:backdrop-blur-xs"
+			onClick={onDialogClick}
 		>
-			<div className="flex-col flex">
+			<div className="py-6 px-10 flex-col flex row-start-1 col-start-1">
 				<div className="flex flex-row gap-4 items-center my-4">
 					<div className="text-orange-500 scale-150">
 						<InfoIcon></InfoIcon>
@@ -46,6 +59,18 @@ export const WarningDialog: React.FC<WarningDialogProps> = ({
 
 				<div className="self-end my-2 lg:m-0">
 					<PrimaryButton label={confirmTitle} onClick={onClick} />
+				</div>
+			</div>
+			<div className="absolute p-2 top-0 right-0">
+				<div className="flex w-full justify-end">
+					<button
+						className="pointer-events-auto p-2"
+						onClick={() => {
+							(document.getElementById(id) as HTMLDialogElement).close();
+						}}
+					>
+						<CloseIcon />
+					</button>
 				</div>
 			</div>
 		</dialog>
