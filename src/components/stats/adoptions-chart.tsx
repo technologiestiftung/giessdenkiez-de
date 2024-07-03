302cff32da6311c269f43fb6556cb36ce08ed1f2
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-07-03 11:33:33 +0200

    fix: use dynamic color names in inline style

--- src/components/stats/adoptions-chart.tsx
+++ src/components/stats/adoptions-chart.tsx
@@ -12,14 +12,23 @@ export const AdoptionsChart: React.FC<AdoptionsChartProps> = ({
 		<div className="w-full">
 			<div className="w-full grid grid-cols-1 grid-rows-">
 				<div
-					className={`w-full h-7 bg-[${adoptionsFillColor}] opacity-[30%] rounded-full row-start-1 col-start-1`}
+					style={{ backgroundColor: adoptionsFillColor }}
+					className={`w-full h-7  opacity-[30%] rounded-full row-start-1 col-start-1`}
 				></div>
 				<div
-					style={{ width: `${veryThirstyAdoptionsRate}%` }}
-					className={`h-7 bg-[${adoptionsFillColor}] rounded-full row-start-1 col-start-1`}
+					style={{
+						width: `${veryThirstyAdoptionsRate}%`,
+						backgroundColor: adoptionsFillColor,
+					}}
+					className={`h-7 rounded-full row-start-1 col-start-1`}
 				></div>
-				<div className={`text-[${adoptionsFillColor}] text-4xl pt-2`}>↑</div>
-				<div className={`text-[${adoptionsFillColor}] text-xl pt-2`}>
+				<div style={{ color: adoptionsFillColor }} className={` text-4xl pt-2`}>
+					↑
+				</div>
+				<div
+					style={{ color: adoptionsFillColor }}
+					className={`text-[${adoptionsFillColor}] text-xl pt-2`}
+				>
 					<span className="font-bold">{veryThirstyAdoptionsRate}%</span> der
 					adoptierten Bäume sind besonders durstig.
 				</div>
