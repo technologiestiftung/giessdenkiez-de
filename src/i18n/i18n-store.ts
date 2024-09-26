d3061a733fb7774ef88f69f839a3cb0992f29fe5
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-09-26 15:22:59 +0200

    feat: persist language selection

--- src/i18n/i18n-store.ts
+++ src/i18n/i18n-store.ts
@@ -2,6 +2,7 @@ import { create } from "zustand";
 import { Content } from "./content-types";
 import { de } from "./de";
 import { en } from "./en";
+import { persist } from "zustand/middleware";
 
 interface LocalizedContent {
 	en: Content;
@@ -18,26 +19,38 @@ interface I18NState {
 	i18n: () => Content;
 }
 
-export const useI18nStore = create<I18NState>()((set, get) => ({
-	language: "de",
-	setLanguage: (language: string) => {
-		set({ language });
-	},
-	formatNumber: (number: number, round?: boolean) => {
-		if (round) {
-			return new Intl.NumberFormat(get().language).format(Math.round(number));
-		}
-		return new Intl.NumberFormat(get().language).format(number);
-	},
-	formatDate: (date: Date) => {
-		return date.toLocaleDateString(get().language, {
-			dateStyle: "full",
-		});
-	},
-	i18n: () => {
-		return (
-			localizedContent[get().language as keyof LocalizedContent] ??
-			localizedContent["de"]
-		);
-	},
-}));
+export const useI18nStore = create<I18NState>()(
+	persist(
+		(set, get) => ({
+			language: "de",
+			setLanguage: (language: string) => {
+				set({ language });
+			},
+			formatNumber: (number: number, round?: boolean) => {
+				if (round) {
+					return new Intl.NumberFormat(get().language).format(
+						Math.round(number),
+					);
+				}
+				return new Intl.NumberFormat(get().language).format(number);
+			},
+			formatDate: (date: Date) => {
+				return date.toLocaleDateString(get().language, {
+					dateStyle: "full",
+				});
+			},
+			i18n: () => {
+				return (
+					localizedContent[get().language as keyof LocalizedContent] ??
+					localizedContent["de"]
+				);
+			},
+		}),
+		{ name: "i18n-store" },
+	),
+);
+
+const languageFilter = new URL(window.location.href).searchParams.get("lang");
+if (languageFilter) {
+	useI18nStore.getState().setLanguage(languageFilter);
+}
