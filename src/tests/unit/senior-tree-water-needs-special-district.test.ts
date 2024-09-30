8c1d1014a857987abfd2da8b216476fb3466445b
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-09-30 15:47:13 +0200

    feat: tests for special trees

---
+++ src/tests/unit/senior-tree-water-needs-special-district.test.ts
@@ -0,0 +1,89 @@
+import { expect, test } from "vitest";
+import { useTreeAgeClassification } from "../../components/tree-detail/hooks/use-tree-age-classification";
+import { useTreeWaterNeedsData } from "../../components/tree-detail/hooks/use-tree-water-needs-data";
+import {
+	TreeAgeClassification,
+	TreeCoreData,
+} from "../../components/tree-detail/tree-types";
+
+test("should calculate correct water needs for senior tree in special district", () => {
+	const treeData: TreeCoreData = {
+		id: "00008100:0014f6e0",
+		lat: "13.40601",
+		lng: "52.53168",
+		art_dtsch: "Saulen-Hainbuche 'Frans Fontaine'",
+		art_bot: "Carpinus betulus 'Frans Fontaine'",
+		gattung_deutsch: "HAINBUCHE",
+		gattung: "CARPINUS",
+		pflanzjahr: 2011,
+		standalter: "12",
+		baumhoehe: "16",
+		bezirk: "Mitte",
+		eigentuemer: "Land Berlin",
+		radolan_sum: 281,
+		radolan_days: [
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 3, 1, 0, 2, 6, 9, 0, 1, 0, 4,
+			4, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 5, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 12, 2, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 2, 2, 2, 1, 0, 0, 5, 2, 5,
+			8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 2, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 7,
+			7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4, 5, 10,
+		],
+		geom: {
+			coordinates: [13.40601, 52.53168],
+		},
+		caretaker: null,
+	};
+
+	const { treeAgeClassification, treeAge } = useTreeAgeClassification(
+		treeData,
+		new Date("2024-01-01T00:00+00:00"),
+	);
+	expect(treeAge).toBe(13);
+	expect(treeAgeClassification).toBe(TreeAgeClassification.SENIOR);
+
+	const {
+		rainSum,
+		rainPercentage,
+		wateringSum,
+		wateringPercentage,
+		otherWateringPercentage,
+		referenceWaterAmount,
+		shouldBeWatered,
+		waterParts,
+	} = useTreeWaterNeedsData(treeData, [], treeAgeClassification);
+
+	expect(rainSum).toBe(15.3);
+	expect(wateringSum).toBe(0);
+	expect(referenceWaterAmount).toBe(300);
+	expect(rainPercentage).toBe(0.051000000000000004);
+	expect(wateringPercentage).toBe(0);
+	expect(otherWateringPercentage).toBe(0.949);
+	expect(shouldBeWatered).toBe(false);
+	expect(waterParts.map((p) => p.progress)).toEqual([
+		0.051000000000000004, 0, 0.949,
+	]);
+});
